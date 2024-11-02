04d94d06575fa7a42cd2d0cf250de6e99f3341b5
hostadarsh <112804576+hostadarsh@users.noreply.github.com>
2024-11-02 14:12:16 +0530

    Time: 1 ms (96.81%) | Memory: 41.8 MB (72.21%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -4,6 +4,11 @@ class Solution {
         if(sentence.charAt(0) != sentence.charAt(sentence.length()-1)){
             return false;
         }
+
+        int k = sentence.indexOf(" ");
+        if(k == -1){
+            return true;
+        }
         
         for(int i = 1; i < sentence.length()-1; i++){
             if(sentence.charAt(i) == ' '){
