8693e2d2da1fbd48332e57347867f4deed65a589
hostadarsh <112804576+hostadarsh@users.noreply.github.com>
2024-11-04 18:06:01 +0530

    Time: 19 ms (58.47%) | Memory: 45.8 MB (59.79%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -1,34 +1,71 @@
-class Solution {
-    public String compressedString(String word) {
+// class Solution {
+//     public String compressedString(String word) {
 
-        StringBuilder ans = new StringBuilder();
-        char prev = word.charAt(0);
-        int count = 1;
+//         StringBuilder ans = new StringBuilder();
+//         char prev = word.charAt(0);
+//         int count = 1;
 
-        for(int i = 1 ; i < word.length(); i++){
-            if(prev != word.charAt(i)){
-                ans.append(count);
-                ans.append(prev);
-                count = 1;
-                prev = word.charAt(i);
+//         for(int i = 1 ; i < word.length(); i++){
+//             if(prev != word.charAt(i)){
+//                 ans.append(count);
+//                 ans.append(prev);
+//                 count = 1;
+//                 prev = word.charAt(i);
+//             }
+//             else {
+//                 if(count == 9){
+//                 ans.append(count);
+//                 ans.append(prev);
+//                 count = 1;
+//             }
+
+//             else{
+//                 count++;
+//             }
+//         }
+        
+//     }
+
+//     ans.append(count);
+//     ans.append(prev);
+
+//     return ans.toString();
+// }
+// }
+
+ class Solution {
+ public String compressedString(String word) {
+
+    StringBuilder ans = new StringBuilder();
+    char pre = word.charAt(0);
+    int count = 1;
+
+    for(int i = 1; i < word.length(); i++){
+        if(word.charAt(i) != pre){
+            ans.append(count);
+            ans.append(pre);
+            pre = word.charAt(i);
+            count = 1;
+        }
+        else{
+            if(count == 9){
+            ans.append(count);
+            ans.append(pre);
+            count = 1;
+
             }
-            else {
-                if(count == 9){
-                ans.append(count);
-                ans.append(prev);
-                count = 1;
-            }
-
             else{
                 count++;
             }
         }
-        
     }
 
-    ans.append(count);
-    ans.append(prev);
+        ans.append(count);
+        ans.append(pre);
 
-    return ans.toString();
-}
-}
+        return ans.toString();
+    
+    
+
+ }
+ }
