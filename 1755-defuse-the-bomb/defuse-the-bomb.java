79412beb4acd522d4c2114ea8ea11fa9c5962013
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2024-11-18 19:32:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (72.20%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] result = new int[n];
+
+        int start = 0, end = 0;
+
+        if(k == 0){
+           //result = Arrays.stream(result).map(x -> 0).toArray();
+            return result;
+        }
+        
+        if(k > 0){
+            start = 1;
+            end = k;
+        }
+        else{
+            //(k < 0){
+            start = n - Math.abs(k);
+            end = n - 1;
+        }
+
+        int sum = 0;
+        for(int i = start; i<= end; i++){
+                sum = sum + code[i];
+        }
+
+        for(int i = 0; i < n; i++){
+            result[i] = sum;
+
+            sum = sum - code[(start)%n];
+            sum = sum + code[(end+1)%n];
+            start++;
+            end++;
+            
+        }
+        return result;
+
+    }
+}
