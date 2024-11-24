5bd05f9f23326ede907e7d01bb7ecd5091cca5c1
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2024-11-25 03:59:24 +0530

    Time: 4 ms (99.19%) | Memory: 55.1 MB (34.30%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -1,23 +1,49 @@
+// class Solution {
+//     public long maxMatrixSum(int[][] matrix) {
+//         int neg = 0;
+//         long sum = 0;
+//         long minVal = Long.MAX_VALUE;
+
+//         for (int[] row : matrix) {
+//             for (int val : row) {
+//                 if (val < 0) {
+//                     neg++;
+//                 }
+//                 sum += Math.abs(val);
+//                 minVal = Math.min(minVal, Math.abs(val));
+//             }
+//         }
+
+//         if (neg % 2 == 1) {
+//             sum -= 2 * minVal;
+//         }
+
+//         return sum;
+//     }
+// }
+
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        int neg = 0;
-        long sum = 0;
-        long minVal = Long.MAX_VALUE;
+        long s=0;
+        int r=matrix.length;
+        int c=matrix[0].length;
+        int m=Integer.MAX_VALUE;
+        boolean n=false;
 
-        for (int[] row : matrix) {
-            for (int val : row) {
-                if (val < 0) {
-                    neg++;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                int v=matrix[i][j];
+                if(v<0){
+                    n=!n;
+                    v=-v;
                 }
-                sum += Math.abs(val);
-                minVal = Math.min(minVal, Math.abs(val));
+                s+=v;
+                if (v<m){
+                    m=v;
+                }
             }
         }
-
-        if (neg % 2 == 1) {
-            sum -= 2 * minVal;
-        }
-
-        return sum;
+        if(n)return s-m*2;
+        return s;
     }
 }
