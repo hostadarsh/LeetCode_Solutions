9aad9ff00330582280f03d575de520b7a1d4e68b
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2024-12-16 19:39:53 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (20.61%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,23 +1,44 @@
-class Solution {
-    public int[] getFinalState(int[] nums, int k, int multiplier) {
-        for(int i = 0 ; i < k; i++){
-        int idx = smallest(nums);
-        nums[idx] = nums[idx] * multiplier;
-        }
+// class Solution {
+//     public int[] getFinalState(int[] nums, int k, int multiplier) {
+//         for(int i = 0 ; i < k; i++){
+//         int idx = smallest(nums);
+//         nums[idx] = nums[idx] * multiplier;
+//         }
 
-        return nums;
+//         return nums;
 
         
-    }
-    public static int smallest(int [] arr){
-        int small = 10000;
-        int idx = 0;
-        for(int i = 0; i< arr.length; i++){
-            if(arr[i] < small){
-                small = arr[i];
-                idx = i;
+//     }
+//     public static int smallest(int [] arr){
+//         int small = 10000;
+//         int idx = 0;
+//         for(int i = 0; i< arr.length; i++){
+//             if(arr[i] < small){
+//                 small = arr[i];
+//                 idx = i;
+//             }
+//         }
+//         return idx;
+//     }
+// }
+
+ class Solution {
+     public int[] getFinalState(int[] arr, int k, int multiplier) {
+
+        int[] ans = new int[arr.length];
+
+        while(k > 0){
+            int x = 0;
+
+            for(int i = 1; i< arr.length; i++){
+                if(arr[i] < arr[x]){
+                    x = i;
+                }
             }
+            arr[x] = arr[x] * multiplier;
+            k--;
         }
-        return idx;
-    }
-}
+        return arr;
+
+     }
+ }
