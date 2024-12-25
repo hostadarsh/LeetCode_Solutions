c1dbcbe59019add737c930ecb25d8991a77d72ea
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2024-12-26 00:26:25 +0530

    Time: 1 ms (94.48%) | Memory: 45.6 MB (31.69%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -13,26 +13,48 @@
  *     }
  * }
  */
-class Solution {
-    private List<Integer> res;
-    public List<Integer> largestValues(TreeNode root) {
-        res = new ArrayList<>();
-        dfs(root, 0);
+// class Solution {
+//     private List<Integer> res;
+//     public List<Integer> largestValues(TreeNode root) {
+//         res = new ArrayList<>();
+//         dfs(root, 0);
 
-        return res;
+//         return res;
+//     }
+
+//     private void dfs(TreeNode root, int level) {
+//         if (root != null) {
+//             int val = root.val;
+            
+//             if (res.size() == level) 
+//                 res.add(val);
+//             else 
+//                 res.set(level, Math.max(res.get(level), val));
+            
+//             dfs(root.left, level + 1);
+//             dfs(root.right, level + 1);
+//         }
+//     }
+// }
+
+class Solution {
+    List<Integer> ans;
+    public List<Integer> largestValues(TreeNode root) {
+        ans = new ArrayList<>();
+        dfs(root, 0);
+        return ans;
     }
 
-    private void dfs(TreeNode root, int level) {
-        if (root != null) {
-            int val = root.val;
-            
-            if (res.size() == level) 
-                res.add(val);
-            else 
-                res.set(level, Math.max(res.get(level), val));
-            
-            dfs(root.left, level + 1);
-            dfs(root.right, level + 1);
+    private void dfs(TreeNode node, int l) {
+        if(node != null) {
+            int v = node.val;
+            if(l == ans.size()) {
+                ans.add(v);
+            } else {
+                ans.set(l, Math.max(ans.get(l), v));
+            }
+            dfs(node.left, l + 1);
+            dfs(node.right, l + 1);
         }
     }
 }
