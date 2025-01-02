929ecc4243ce24ddbeb835d2f8365ad59063e7b3
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-01-02 22:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (50.05%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,17 +1,41 @@
+// class Solution {
+//     public int maxScore(String s) {
+//         int totalZeros = 0;
+//         for (char c : s.toCharArray()) {
+//             if (c == '0') totalZeros++;
+//         }
+//         int ans = -1, zeros = 0, n = s.length();
+//         for (int i = 1; i < n; i++) {
+//             if (s.charAt(i - 1) == '0') {
+//                 zeros++;
+//                 totalZeros--;
+//             }
+//             ans = Math.max(ans, zeros + (n - totalZeros - i));
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int maxScore(String s) {
-        int totalZeros = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '0') totalZeros++;
-        }
-        int ans = -1, zeros = 0, n = s.length();
-        for (int i = 1; i < n; i++) {
-            if (s.charAt(i - 1) == '0') {
-                zeros++;
-                totalZeros--;
+        int n = s.length();
+        int ons = 0;
+        int curr = s.charAt(0) == '0' ? 1 : 0;
+        int score = curr;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                curr++;
+            } else {
+                ons++;
+                curr--;
             }
-            ans = Math.max(ans, zeros + (n - totalZeros - i));
+            if (curr > score) {
+                score = curr;
+            }
         }
-        return ans;
+        ons += s.charAt(n - 1) == '1' ? 1 : 0;
+
+        return ons + score;
     }
 }
