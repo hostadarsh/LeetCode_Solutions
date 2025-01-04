3b5bd8c0ff551421422aeec01d90032b76db8126
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-01-04 22:54:17 +0530

    Time: 3 ms (100.00%) | Memory: 45.7 MB (39.53%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,23 +1,66 @@
+// class Solution {
+//     public int countPalindromicSubsequence(String s) {
+//         int[] R = new int[26];
+//         for (char u : s.toCharArray()) {
+//             R[u - 'a']++;
+//         }
+//         int[] L = new int[26];
+//         HashSet<Integer> S = new HashSet<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             int t = s.charAt(i) - 'a';
+//             R[t]--;
+//             for (int j = 0; j < 26; j++) {
+//                 if (L[j] > 0 && R[j] > 0) {
+//                     S.add(26 * t + j);
+//                 }
+//             }
+//             L[t]++;
+//         }
+
+//         return S.size();
+//     }
+// }
+
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int[] R = new int[26];
-        for (char u : s.toCharArray()) {
-            R[u - 'a']++;
-        }
-        int[] L = new int[26];
-        HashSet<Integer> S = new HashSet<>();
+        final int n = s.length();
+        final byte[] sc = new byte[n];
+        s.getBytes(0, n, sc, 0);
+        
+        final int[][] firstCounts = new int[26][26];
+        final int[] firstIdx = new int[26];
+        final int[] charCounts = new int[26];
+        int foundLettersCount = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            int t = s.charAt(i) - 'a';
-            R[t]--;
-            for (int j = 0; j < 26; j++) {
-                if (L[j] > 0 && R[j] > 0) {
-                    S.add(26 * t + j);
-                }
+        for (int i = 0; i < n; i++) {
+            final int c = sc[i] - 'a';
+            charCounts[c]++;
+            if (firstIdx[c] == 0) {
+                firstIdx[c] = i + 1;
+                final int[] firsts = firstCounts[c];
+                for (int j = 0; j < 26; j++) 
+                    firsts[j] = charCounts[j];
+                foundLettersCount++;
             }
-            L[t]++;
         }
 
-        return S.size();
+        int palindromeCount = 0;
+        final boolean[] foundLast = new boolean[26];
+        for (int i = n - 1; i >= 2; i--) {
+            final int c = sc[i] - 'a';
+            charCounts[c]--;
+            if (!foundLast[c]) {
+                foundLast[c] = true;
+                if (i > firstIdx[c]) {
+                    final int[] firsts = firstCounts[c];
+                    for (int j = 0; j < 26; j++) 
+                        if (charCounts[j] > firsts[j]) 
+                            palindromeCount++;
+                }
+                if (--foundLettersCount == 0)  break;
+            }
+        }
+        return palindromeCount;
     }
 }
