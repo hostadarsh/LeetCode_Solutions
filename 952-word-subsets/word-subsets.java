3e727740ae176d03332f3d047d81ddfdb7abfcd4
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-01-12 01:00:46 +0530

    Time: 14 ms (49.70%) | Memory: 52.6 MB (99.41%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,38 +1,98 @@
-class Solution {
-    public List<String> wordSubsets(String[] words1, String[] words2) {
+// class Solution {
+//     public List<String> wordSubsets(String[] words1, String[] words2) {
+//         int[] maxCharFreq = new int[26];
+//         int[] tempCharFreq = new int[26];
+
+//         for (String word : words2) {
+//             Arrays.fill(tempCharFreq, 0);
+//             for (char ch : word.toCharArray()) {
+//                 tempCharFreq[ch - 'a']++;
+//             }
+//             for (int i = 0; i < 26; ++i) {
+//                 maxCharFreq[i] = Math.max(maxCharFreq[i], tempCharFreq[i]);
+//             }
+//         }
+
+//         List<String> universalWords = new ArrayList<>();
+
+//         for (String word : words1) {
+//             Arrays.fill(tempCharFreq, 0);
+//             for (char ch : word.toCharArray()) {
+//                 tempCharFreq[ch - 'a']++;
+//             }
+
+//             boolean isUniversal = true;
+//             for (int i = 0; i < 26; ++i) {
+//                 if (maxCharFreq[i] > tempCharFreq[i]) {
+//                     isUniversal = false;
+//                     break;
+//                 }
+//             }
+//             if (isUniversal) {
+//                 universalWords.add(word);
+//             }
+//         }
+
+//         return universalWords;
+//     }
+// }
+
+ class Solution {
+     public List<String> wordSubsets(String[] words1, String[] words2) {
+        
         int[] maxCharFreq = new int[26];
         int[] tempCharFreq = new int[26];
 
-        for (String word : words2) {
-            Arrays.fill(tempCharFreq, 0);
-            for (char ch : word.toCharArray()) {
+        for(String word : words2){
+            Arrays.fill(tempCharFreq,0);
+            for(char ch : word.toCharArray()){
                 tempCharFreq[ch - 'a']++;
             }
-            for (int i = 0; i < 26; ++i) {
+            for(int i = 0 ; i< 26;i++){
                 maxCharFreq[i] = Math.max(maxCharFreq[i], tempCharFreq[i]);
             }
         }
 
-        List<String> universalWords = new ArrayList<>();
+        List<String> Universal = new ArrayList<>();
 
-        for (String word : words1) {
-            Arrays.fill(tempCharFreq, 0);
-            for (char ch : word.toCharArray()) {
+        for(String word: words1){
+            Arrays.fill(tempCharFreq,0);
+            for(char ch : word.toCharArray()){
                 tempCharFreq[ch - 'a']++;
             }
 
             boolean isUniversal = true;
-            for (int i = 0; i < 26; ++i) {
-                if (maxCharFreq[i] > tempCharFreq[i]) {
+
+            for(int i = 0 ; i< 26; i++){
+                if(maxCharFreq[i] > tempCharFreq[i]){
                     isUniversal = false;
                     break;
                 }
             }
-            if (isUniversal) {
-                universalWords.add(word);
-            }
-        }
 
-        return universalWords;
+            if(isUniversal){
+                Universal.add(word);
+            }
+            
+        }
+        return Universal;
+
     }
-}
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
