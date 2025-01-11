8110ffb67f28bce13a3eea5525629346f4cf523d
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-01-12 01:09:21 +0530

    Time: 3 ms (100.00%) | Memory: 45.6 MB (19.16%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,23 +1,42 @@
-import java.util.Arrays;
+// import java.util.Arrays;
+
+// class Solution {
+//     public boolean canConstruct(String s, int k) {
+//         if (s.length() < k) return false;
+
+//         char[] chars = s.toCharArray();
+//         Arrays.sort(chars);
+//         int oddCount = 0;
+
+//         for (int i = 0; i < chars.length; ) {
+//             char current = chars[i];
+//             int count = 0;
+//             while (i < chars.length && chars[i] == current) {
+//                 count++;
+//                 i++;
+//             }
+//             if (count % 2 != 0) oddCount++;
+//         }
+
+//         return oddCount <= k;
+//     }
+// }
 
 class Solution {
     public boolean canConstruct(String s, int k) {
+        // Handle edge cases
         if (s.length() < k) return false;
+        if (s.length() == k) return true;
 
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
+        // Initialize oddCount as an integer bitmask
         int oddCount = 0;
 
-        for (int i = 0; i < chars.length; ) {
-            char current = chars[i];
-            int count = 0;
-            while (i < chars.length && chars[i] == current) {
-                count++;
-                i++;
-            }
-            if (count % 2 != 0) oddCount++;
+        // Update the bitmask for each character in the string
+        for (char chr : s.toCharArray()) {
+            oddCount ^= 1 << (chr - 'a');
         }
 
-        return oddCount <= k;
+        // Return if the number of odd frequencies is less than or equal to k
+        return Integer.bitCount(oddCount) <= k;
     }
 }
