24bc5a9491bddad4e685577e8dae8c566dbb459e
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-01-13 21:15:17 +0530

    Time: 8 ms (100.00%) | Memory: 46.3 MB (66.89%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,26 +1,44 @@
+// class Solution {
+//     public int minimumLength(String s) {
+
+//         int[] maxCharFreq = new int[26];
+//         int result = 0;
+//         int totalFrequency = 0;
+
+//         for(char ch : s.toCharArray()){
+//             maxCharFreq[ch-'a']++;
+//         }
+
+//         for(int frequency : maxCharFreq){
+//             if(frequency == 0){
+//                 continue;
+//             }
+//             if(frequency % 2 == 0){
+//                 result += 2;
+//             }
+//             else{
+//                 result +=1;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
-    public int minimumLength(String s) {
-
-        int[] maxCharFreq = new int[26];
-        int result = 0;
-        int totalFrequency = 0;
-
-        for(char ch : s.toCharArray()){
-            maxCharFreq[ch-'a']++;
+    static {
+        for(int i = 0; i < 100; i++) {
+            minimumLength("qbbqq");
         }
-
-        for(int frequency : maxCharFreq){
-            if(frequency == 0){
-                continue;
-            }
-            if(frequency % 2 == 0){
-                result += 2;
-            }
-            else{
-                result +=1;
-            }
+    }
+    public static int minimumLength(String s) {
+        int[] freq = new int[26];
+        for(byte c: s.getBytes()) freq[c - 'a']++;
+        int res = 0;
+        for(int i: freq) {
+            if (i == 0) continue;
+            res += (i & 1 ^ 1) << 1 | i & 1;
         }
-        return result;
+        return res;
     }
 }
-
