0819045544cfcca9e70214321d1d3217e36a8c37
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-01-23 15:31:44 +0530

    Time: 2 ms (100.00%) | Memory: 52.9 MB (20.85%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -1,21 +1,48 @@
+// class Solution {
+//     public int countServers(int[][] grid) {
+//         int[] Rows = new int[grid.length];
+//         int[] Col = new int[grid[0].length];
+//         for (int i = 0; i < grid.length; i++) {
+//             for (int j = 0; j < grid[0].length; j++) {
+//                 Rows[i] += grid[i][j];
+//                 Col[j] += grid[i][j];
+//             }
+//         }
+//         int ans = 0;
+//         for (int i = 0; i < grid.length; i++) {
+//             for (int j = 0; j < grid[0].length; j++) {
+//                 if (grid[i][j] == 1 && (Rows[i] > 1 || Col[j] > 1)) {
+//                     ans++;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int countServers(int[][] grid) {
-        int[] Rows = new int[grid.length];
-        int[] Col = new int[grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                Rows[i] += grid[i][j];
-                Col[j] += grid[i][j];
-            }
-        }
-        int ans = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1 && (Rows[i] > 1 || Col[j] > 1)) {
-                    ans++;
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
                 }
             }
         }
-        return ans;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
