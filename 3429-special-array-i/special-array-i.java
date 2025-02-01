580aa56f05f4d9f3c706eba97cb45d0817ae9a6a
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-02-01 23:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (84.31%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,18 +1,31 @@
-class Solution {
-    public boolean isArraySpecial(int[] nums) {
+// class Solution {
+//     public boolean isArraySpecial(int[] nums) {
 
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] % 2 == 0 && nums[i+1] % 2 != 0){
-                continue;
-            }
-            else if(nums[i] % 2 != 0 && nums[i+1] % 2 == 0){
-                continue;
-            }
-            else{
-                return false;
-            }
+//         for(int i = 0; i < nums.length-1; i++){
+//             if(nums[i] % 2 == 0 && nums[i+1] % 2 != 0){
+//                 continue;
+//             }
+//             else if(nums[i] % 2 != 0 && nums[i+1] % 2 == 0){
+//                 continue;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+//     }
+// }
+
+class Solution {
+    public boolean isArraySpecial(int[] nums){
+        for(int i = 0; i< nums.length-1; i++){
+        if(nums[i]%2 == nums[i+1]%2){
+            return false;
+        }
+       
         }
         return true;
-        
+
     }
 }
