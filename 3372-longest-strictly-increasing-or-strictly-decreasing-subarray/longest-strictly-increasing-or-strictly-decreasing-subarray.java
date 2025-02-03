60690dd44403ddaf69b58b2da72d5ed914f996d3
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-02-04 01:05:10 +0530

    Time: 1 ms (94.12%) | Memory: 43.5 MB (6.21%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,24 +1,47 @@
+// class Solution {
+//     public int longestMonotonicSubarray(int[] nums) {
+//         if(nums.length == 0){
+//             return 0;
+//         }
+
+//         int maxLen = 1, inc = 1, dec = 1;
+//         for(int i = 1; i < nums.length;i++){
+//             if(nums[i] > nums[i-1]){
+//                 inc++;
+//                 dec =1;
+//             }
+//             else if(nums[i]< nums[i-1]){
+//                 dec++;
+//                 inc = 1;
+//             }
+//             else{
+//                 inc = 1; dec = 1;
+//             }
+//             maxLen = Math.max(maxLen, Math.max(inc,dec));
+//         }
+//         return maxLen;
+//     }
+// }
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
+        int incLength = 1;
+        int decLength = 1;
+        int maxLength = 1;
 
-        int maxLen = 1, inc = 1, dec = 1;
-        for(int i = 1; i < nums.length;i++){
-            if(nums[i] > nums[i-1]){
-                inc++;
-                dec =1;
+        for(int pos = 0; pos < nums.length - 1; pos++) {
+            if(nums[pos + 1] > nums[pos]) {
+                incLength++;
+                decLength = 1;
+            } else if(nums[pos + 1] < nums[pos]) {
+                decLength++;
+                incLength = 1;
+            } else {
+                incLength = 1;
+                decLength = 1;
             }
-            else if(nums[i]< nums[i-1]){
-                dec++;
-                inc = 1;
-            }
-            else{
-                inc = 1; dec = 1;
-            }
-            maxLen = Math.max(maxLen, Math.max(inc,dec));
+            maxLength = Math.max(maxLength, Math.max(incLength, decLength));
         }
-        return maxLen;
+        return maxLength;
     }
+
 }
