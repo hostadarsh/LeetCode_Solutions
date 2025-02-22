2f352eee061f020823e16307f8c27f5027d5b843
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-02-23 02:08:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (57.60%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,24 +1,42 @@
+// class Solution {
+//     public int[] finalPrices(int[] prices) {
+//         int n=prices.length;
+//         Stack<Integer> st=new Stack();
+
+//         for(int i=n-1; i>=0; i--){
+//             // Remove elements from the stack that are greater than the current price
+//             while(!st.isEmpty() && st.peek()>prices[i]){
+//                 st.pop();
+//             }
+            
+//             // Get the discount from the stack or 0 if no valid discount
+//             int discount=st.isEmpty()?0:st.peek();
+            
+//             // Push the original value before updating
+//             st.push(prices[i]);
+
+//             // Update the current price with the discount
+//             prices[i]-=discount;           
+//         }
+
+//         return prices;
+//     }
+// }
+
 class Solution {
-    public int[] finalPrices(int[] prices) {
-        int n=prices.length;
-        Stack<Integer> st=new Stack();
-
-        for(int i=n-1; i>=0; i--){
-            // Remove elements from the stack that are greater than the current price
-            while(!st.isEmpty() && st.peek()>prices[i]){
-                st.pop();
+    private int getDiscountedPrice(int index, int[] prices) {
+        for (int i = index + 1; i < prices.length; i++) {
+            if (prices[index] >= prices[i]) {
+                return prices[index] - prices[i];
             }
-            
-            // Get the discount from the stack or 0 if no valid discount
-            int discount=st.isEmpty()?0:st.peek();
-            
-            // Push the original value before updating
-            st.push(prices[i]);
-
-            // Update the current price with the discount
-            prices[i]-=discount;           
         }
-
-        return prices;
+        return prices[index];
+    }
+    public int[] finalPrices(int[] prices) {
+        int[] result = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            result[i] = getDiscountedPrice(i, prices);
+        }
+        return result;
     }
 }
