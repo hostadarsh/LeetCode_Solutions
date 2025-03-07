3cfbb814b6c2d4890c8de1de21722a8d81b50646
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-07 12:12:48 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (64.16%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int idx = 0; 
+       // int[] res = nums;  - we dont use this because it only give the reference of nums to res and not actually copy the array
+        int[] res = Arrays.copyOf(nums,nums.length);
+
+         for(int i = 0; i < 3; i++){
+            for(int j = 0; j < nums.length; j++ ){
+                if(res[j] == i){
+                    nums[idx] = i;
+                    idx++;
+                }
+            }
+        }
+        
+        
+    }
+}
