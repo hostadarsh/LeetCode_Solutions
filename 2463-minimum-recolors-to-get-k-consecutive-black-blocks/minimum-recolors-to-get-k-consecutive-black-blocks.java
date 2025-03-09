0800e8174fe3911bf3879f9cff8eb71592a04cb4
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-09 23:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (86.63%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,21 +1,42 @@
+// class Solution {
+//     public int minimumRecolors(String blocks, int k) {
+//         //int min = 0;
+//         int count = 0;
+//         int[] arr = new int[blocks.length()-k+1];
+
+//         for(int i = 0; i <= blocks.length() - k; i++){
+            
+//             for(int j = i; j < i + k ; j++){
+//                 if(blocks.charAt(j) == 'W'){
+//                     count++;
+//                 }
+//             }
+//             arr[i] = count;
+//             count = 0;
+//         }
+
+//         return Arrays.stream(arr).min().getAsInt();
+        
+//     }
+// }
+
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        //int min = 0;
-        int count = 0;
-        int[] arr = new int[blocks.length()-k+1];
-
-        for(int i = 0; i <= blocks.length() - k; i++){
-            
-            for(int j = i; j < i + k ; j++){
-                if(blocks.charAt(j) == 'W'){
-                    count++;
-                }
-            }
-            arr[i] = count;
-            count = 0;
+        int opr=0, n=blocks.length(), ans;
+        for(int i=0; i<k; i++){
+            if(blocks.charAt(i)=='W') opr++;
         }
-
-        return Arrays.stream(arr).min().getAsInt();
-        
+        ans=opr;
+        for(int i=k; i<n; i++){
+            if(blocks.charAt(i-k)!=blocks.charAt(i)){
+                if(blocks.charAt(i-k)=='W') opr--;
+                else opr++;
+            }
+            if(ans>opr) {
+                ans=opr;
+                if(ans==0) return ans;
+            }
+        }
+        return ans;
     }
 }
