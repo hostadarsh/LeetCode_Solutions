0b914eee2ca15501fd7d0bdbf1de966ec76339af
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-09 23:52:42 +0530

    Time: 1 ms (100.00%) | Memory: 63.3 MB (37.43%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.java
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -1,19 +1,37 @@
+// class Solution {
+//     public int numberOfAlternatingGroups(int[] colors, int k) {
+//         int n = colors.length;
+//         int count = 0;
+//         int left = 0;
+        
+//         for (int right = 0; right < n + k - 1; right++) {
+//             if (right > 0 && colors[right % n] == colors[(right - 1) % n]) {
+//                 left = right;  
+//             }
+            
+//             if (right - left + 1 >= k) {
+//                 count++;  
+//             }
+//         }
+        
+//         return count;
+//     }
+// }
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        int n = colors.length;
-        int count = 0;
-        int left = 0;
-        
-        for (int right = 0; right < n + k - 1; right++) {
-            if (right > 0 && colors[right % n] == colors[(right - 1) % n]) {
-                left = right;  
+        int cnt = 1;
+        int res = 0;
+        int firstcnt = 0;
+        for (int i = 1; i < colors.length; i++) {
+            if (colors[i] != colors[i-1]) cnt++;
+            else {
+                if (firstcnt == 0) firstcnt = Math.min(k-1, cnt); 
+                cnt = 1;
             }
-            
-            if (right - left + 1 >= k) {
-                count++;  
-            }
+            if (cnt >= k) res++;
         }
-        
-        return count;
+        if (colors[colors.length-1] == colors[0]) cnt = -1000;
+        if (cnt == colors.length) return cnt;
+        return res + Math.max(0, firstcnt + Math.min(cnt, k-1) - k + 1);
     }
 }
