a4e79a3e4185c69c1997192e8b609a99a51534ce
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-14 03:20:31 +0530

    Time: 20 ms (50.00%) | Memory: 94.2 MB (96.33%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -30,28 +30,63 @@
 //     }
 // }
 
+// class Solution {
+//     public int minZeroArray(int[] nums, int[][] queries) {
+//         int n = nums.length;
+//         int[] differenceArray = new int[n + 1];
+
+//         int differentArrayPrefixSum = 0;
+//         int q = 0;
+//         for (int i = 0; i < n; i++) {
+//             while (differentArrayPrefixSum + differenceArray[i] < nums[i]) {
+//                 q++;
+//                 if (q > queries.length) return -1;
+
+//                 int[] nextQuery = queries[q - 1];
+//                 int left = nextQuery[0], right = nextQuery[1], val = nextQuery[2];
+//                 if (right >= i) {
+//                     differenceArray[Math.max(left, i)] += val;
+//                     differenceArray[right + 1] -= val;
+//                 }
+//             }
+//             differentArrayPrefixSum += differenceArray[i];
+//         }
+//         return q;
+//     }
+// }
+
 class Solution {
-    public int minZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
-        int[] differenceArray = new int[n + 1];
-
-        int differentArrayPrefixSum = 0;
-        int q = 0;
-        for (int i = 0; i < n; i++) {
-            while (differentArrayPrefixSum + differenceArray[i] < nums[i]) {
-                q++;
-                if (q > queries.length) return -1;
-
-                int[] nextQuery = queries[q - 1];
-                int left = nextQuery[0], right = nextQuery[1], val = nextQuery[2];
-                if (right >= i) {
-                    differenceArray[Math.max(left, i)] += val;
-                    differenceArray[right + 1] -= val;
-                }
+    private boolean isValid(int m, int[] nums, int[][] queries) {
+        int[] temp = new int[nums.length];
+        for(int i = 0; i <= m; i++) {
+            temp[queries[i][0]] += queries[i][2];
+            if ((queries[i][1]+1) < nums.length) {
+                temp[queries[i][1]+1] -= queries[i][2];
             }
-            differentArrayPrefixSum += differenceArray[i];
         }
-        return q;
+        int cur = 0;
+        for(int i = 0; i < nums.length; i++) { 
+            cur += temp[i];
+            if(nums[i] > cur) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int minZeroArray(int[] nums, int[][] queries) {
+        if(!isValid(queries.length-1, nums, queries)) {
+            return -1;   
+        }
+        int i = -1, j = queries.length-1;
+        while (i < j) {
+            int m = i + (j-i)/2;
+            if (isValid(m, nums, queries)) {
+                j = m;
+            } else {
+                i = m +1;
+            }
+        }
+        return i+1;
     }
 }
 
