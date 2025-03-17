0aef0b78ab11ace9438fcd5d997ac49720bd7a36
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-17 15:43:29 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (58.82%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,16 +1,36 @@
+// class Solution {
+//     public boolean divideArray(int[] nums) {
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         for(int i:nums){
+//             if(map.containsKey(i)){
+//                 map.put(i,map.get(i)+1);
+//             }
+//             else map.put(i,1);
+//         }       
+
+//         for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+//             if(entry.getValue()%2!=0) return false;
+//         }
+
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean divideArray(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i:nums){
-            if(map.containsKey(i)){
-                map.put(i,map.get(i)+1);
-            }
-            else map.put(i,1);
-           }       
 
-        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-            if(entry.getValue()%2!=0) return false;
+        int[] arr = new int[501];
+
+        for(int a : nums){
+            arr[a]++;
+        }
+        
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i]%2 != 0){
+                return false;
+            }
         }
         return true;
+
     }
 }
