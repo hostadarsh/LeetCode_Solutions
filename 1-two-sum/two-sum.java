cbcca5d753a99ada73e538db0401f9de6313d956
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-20 23:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (62.66%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,30 @@
+// import java.util.HashMap;
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+
+//         for(int i = 1; i < nums.length; i++){
+//             for(int j = i; j < nums.length ; j++){
+//                 if(nums[j] + nums[j-1] == target){
+//                     return new int[]{j-1,j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+        
+//     }
+// }
+
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length ; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                   
+     for(int i = 1; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++) {
+                if(nums[j] + nums[j - i] == target) {
+                return new int[]{j-i, j};
                 }
             }
         }
         return new int[]{};
-        
     }
 }
