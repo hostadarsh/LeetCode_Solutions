f70ff33203476321519a1e91525ea6903339ce35
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-22 02:51:16 +0530

    Time: 29 ms (72.41%) | Memory: 51.2 MB (92.58%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -20,41 +20,87 @@
 // }
 
 
+
+
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] arr) {
+
+//         int count = 0;
+//         for(int n : arr){
+//         if(n == 0){
+//             count++;
+//         }
+//         if(count == arr.length){
+//             List<List<Integer>> ans1 = new ArrayList<>();
+//             ans1.add(Arrays.asList(0,0,0));
+//             return ans1;
+//         }
+//         }
+
+//         Set<List<Integer>> st = new HashSet<>();
+
+//         for(int i = 0; i < arr.length; i++ ){
+
+//             Set<Integer> hashset = new HashSet<>();
+
+//             for(int j = i+1; j < arr.length; j++){
+//                 int third = -(arr[i] + arr[j]);
+
+//                 if(hashset.contains(third)){
+//                     List<Integer> temp = new ArrayList<>();
+//                     temp = Arrays.asList(arr[i],arr[j],third);
+//                     temp.sort(null);
+//                     st.add(temp);
+//                 }   
+//                 hashset.add(arr[j]);             
+//             }
+//         }
+//         List<List<Integer>> ans = new ArrayList<>(st);
+//         return ans;
+
+//     }
+// }
+
+
+
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
 
-        int count = 0;
-        for(int n : arr){
-        if(n == 0){
-            count++;
-        }
-        if(count == arr.length){
-            List<List<Integer>> ans1 = new ArrayList<>();
-            ans1.add(Arrays.asList(0,0,0));
-            return ans1;
-        }
-        }
+        List<List<Integer>> ans = new ArrayList<>();
 
-        Set<List<Integer>> st = new HashSet<>();
+        Arrays.sort(arr);
 
-        for(int i = 0; i < arr.length; i++ ){
+        for(int i = 0 ; i< arr.length; i++){
+            if(i != 0 && arr[i] == arr[i-1]){
+                continue;
+            }
 
-            Set<Integer> hashset = new HashSet<>();
+            int j = i+1;
+            int k = arr.length -1;
 
-            for(int j = i+1; j < arr.length; j++){
-                int third = -(arr[i] + arr[j]);
+            while(j<k){
+                int sum = arr[i] + arr[j] + arr[k];
 
-                if(hashset.contains(third)){
-                    List<Integer> temp = new ArrayList<>();
-                    temp = Arrays.asList(arr[i],arr[j],third);
-                    temp.sort(null);
-                    st.add(temp);
-                }   
-                hashset.add(arr[j]);             
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(j<k && arr[j] == arr[j-1]){
+                        j++;
+                    }
+                    while(j<k && arr[k] == arr[k+1]){
+                        k--;
+                    }
+                }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
-
     }
 }
