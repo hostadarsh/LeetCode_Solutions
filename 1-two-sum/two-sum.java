aa4616232096bed0428044572cef8cda0e335613
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-21 20:29:20 +0530

    Time: 2 ms (98.83%) | Memory: 45.2 MB (29.77%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -14,18 +14,46 @@
 //     }
 // }
 
+
+
+
+// import java.util.HashMap;
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//      for(int i = 1; i < nums.length; i++){
+//             for(int j = i; j < nums.length; j++) {
+//                 if(nums[j] + nums[j - i] == target) {
+//                 return new int[]{j-i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
+
+
 import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-     for(int i = 1; i < nums.length; i++){
-            for(int j = i; j < nums.length; j++) {
-                if(nums[j] + nums[j - i] == target) {
-                return new int[]{j-i, j};
-                }
+
+        int [] ans = new int[2];
+
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        
+        for(int i = 0; i< nums.length; i++){
+            int num = nums[i];
+            int moreNeeded = target-num;
+            if(mpp.containsKey(moreNeeded)){
+                ans[0] = mpp.get(moreNeeded);
+                ans[1] = i;
+                return ans;
             }
+            mpp.put(nums[i],i);
         }
-        return new int[]{};
+
+        return ans;
     }
 }
 
