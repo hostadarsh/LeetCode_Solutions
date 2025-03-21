4f2420f3b476bb9d41f7c3d8fe28601c421f472f
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-22 00:24:53 +0530

    Time: 957 ms (7.37%) | Memory: 53 MB (11.49%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,60 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] arr) {
+//         Set<List<Integer>> st = new HashSet<>();
+
+//         for(int i = 0; i < arr.length ; i++){
+//             for(int j = i + 1 ; j < arr.length ; j++){
+//                 for(int k = j + 1; k < arr.length ; k++){
+//                     if(arr[i] + arr[j] + arr[k] == 0){
+//                         List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+//                         temp.sort(null);
+//                         st.add(temp);
+//                     }
+//                 }
+//             }
+//         }
+//         //List<List<Integer>> ans = new ArrayList<>(st);
+//         return new ArrayList<>(st);
+
+//     }
+// }
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+
+        int count = 0;
+        for(int n : arr){
+        if(n == 0){
+            count++;
+        }
+        if(count == arr.length){
+            List<List<Integer>> ans1 = new ArrayList<>();
+            ans1.add(Arrays.asList(0,0,0));
+            return ans1;
+        }
+        }
+
+        Set<List<Integer>> st = new HashSet<>();
+
+        for(int i = 0; i < arr.length; i++ ){
+
+            Set<Integer> hashset = new HashSet<>();
+
+            for(int j = i+1; j < arr.length; j++){
+                int third = -(arr[i] + arr[j]);
+
+                if(hashset.contains(third)){
+                    List<Integer> temp = new ArrayList<>();
+                    temp = Arrays.asList(arr[i],arr[j],third);
+                    temp.sort(null);
+                    st.add(temp);
+                }   
+                hashset.add(arr[j]);             
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+
+    }
+}
