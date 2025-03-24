3214d49a0b2b9e764262614c43ee078483edba53
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-24 06:55:30 +0530

    Time: 16 ms (11.07%) | Memory: 47.8 MB (65.99%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,27 +1,54 @@
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+        
+//         List<Integer> ls = new ArrayList<>();
+//         int s = nums.length;
+
+//         for(int i = 0; i < s; i++ ){
+//             if(ls.size() == 0 || ls.get(0) != nums[i]){
+//                 int count = 0;
+//                 for(int j = 0; j < s; j++){
+//                     if(nums[i] == nums[j]){
+//                         count++;
+//                     }
+//                 }
+//                 if(count > (s/3)){
+//                     ls.add(nums[i]);
+//                 }
+//             }
+
+//             if(ls.size() == 2){
+//                 break;
+//             }
+//         }
+//         return ls;
+
+//     }
+// }
+
+
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        
-        List<Integer> ls = new ArrayList<>();
-        int s = nums.length;
 
-        for(int i = 0; i < s; i++ ){
-            if(ls.size() == 0 || ls.get(0) != nums[i]){
-                int count = 0;
-                for(int j = 0; j < s; j++){
-                    if(nums[i] == nums[j]){
-                        count++;
-                    }
-                }
-                if(count > (s/3)){
-                    ls.add(nums[i]);
-                }
+        List<Integer> ans = new ArrayList<>();
+
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        int min = (int)(nums.length/3) + 1;
+
+        for(int i = 0; i< nums.length; i++){
+            
+            int value = mpp.getOrDefault(nums[i],0);
+            mpp.put(nums[i], value+1);
+
+            if(mpp.get(nums[i]) == min){
+                ans.add(nums[i]);
             }
-
-            if(ls.size() == 2){
+            if(ans.size() == 2){
                 break;
-            }
-        }
-        return ls;
+            }        }
+
+        return ans;
 
     }
 }
