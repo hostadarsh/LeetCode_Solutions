3f0efdd16bb1af8a959398cf3105f7a46327aafb
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-31 01:16:00 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (43.11%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,21 +1,51 @@
+// class Solution {
+//     public List<Integer> partitionLabels(String s) {
+//         Map<Character, Integer> lastOccurrence = new HashMap<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             lastOccurrence.put(s.charAt(i), i);
+//         }
+
+//         List<Integer> result = new ArrayList<>();
+//         int start = 0, end = 0;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             end = Math.max(end, lastOccurrence.get(s.charAt(i)));
+//             if (i == end) {
+//                 result.add(end - start + 1);
+//                 start = i + 1;
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        Map<Character, Integer> lastOccurrence = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            lastOccurrence.put(s.charAt(i), i);
+        int[] lastOccurrence = new int[26];
+        int idx = 0;
+        for (char c : s.toCharArray()) {
+            lastOccurrence[c-'a'] = idx;
+            idx++;
         }
 
-        List<Integer> result = new ArrayList<>();
-        int start = 0, end = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            end = Math.max(end, lastOccurrence.get(s.charAt(i)));
-            if (i == end) {
-                result.add(end - start + 1);
-                start = i + 1;
-            }
+        List<Integer> ans = new ArrayList<>();
+        idx = 0;
+        int st = 0, n = s.length();
+        while (idx < n) {
+            st = getMax(s,lastOccurrence,idx);
+            ans.add(st-idx+1);
+            idx = st+1;
         }
-
-        return result;
+        return ans;
+    }
+    private int getMax(String str, int[] lastOccurence, int s) {
+        int l = Math.max(s,lastOccurence[str.charAt(s)-'a']), m = l;
+        while (s <= Math.max(m,l)) {
+            m = Math.max(m,lastOccurence[str.charAt(s)-'a']);
+            s++;
+        }
+        return m;
     }
 }
