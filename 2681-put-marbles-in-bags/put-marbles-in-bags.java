d8c5058e4865c41bb7b3595afa7cf96af1e748f8
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-03-31 19:54:11 +0530

    Time: 33 ms (84.33%) | Memory: 63.5 MB (32.09%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -1,22 +1,46 @@
+// class Solution {
+//     public long putMarbles(int[] weights, int k) {
+//         if (k == 1) {
+//             return 0;
+//         }
+
+//         List<Integer> pairSums = new ArrayList<>();
+//         for (int i = 0; i < weights.length - 1; i++) {
+//             pairSums.add(weights[i] + weights[i + 1]);
+//         }
+
+//         Collections.sort(pairSums);
+
+//         long minScore = 0, maxScore = 0;
+//         for (int i = 0; i < k - 1; i++) {
+//             minScore += pairSums.get(i);
+//             maxScore += pairSums.get(pairSums.size() - 1 - i);
+//         }
+
+//         return maxScore - minScore;
+//     }
+// }
+
+
 class Solution {
+
     public long putMarbles(int[] weights, int k) {
-        if (k == 1) {
-            return 0;
+        // We collect and sort the value of all n - 1 pairs.
+        int n = weights.length;
+        int[] pairWeights = new int[n - 1];
+        for (int i = 0; i < n - 1; ++i) {
+            pairWeights[i] = weights[i] + weights[i + 1];
+        }
+        // We will sort only the first (n - 1) elements of the array.
+        Arrays.sort(pairWeights, 0, n - 1);
+
+        // Get the difference between the largest k - 1 values and the
+        // smallest k - 1 values.
+        long answer = 0l;
+        for (int i = 0; i < k - 1; ++i) {
+            answer += pairWeights[n - 2 - i] - pairWeights[i];
         }
 
-        List<Integer> pairSums = new ArrayList<>();
-        for (int i = 0; i < weights.length - 1; i++) {
-            pairSums.add(weights[i] + weights[i + 1]);
-        }
-
-        Collections.sort(pairSums);
-
-        long minScore = 0, maxScore = 0;
-        for (int i = 0; i < k - 1; i++) {
-            minScore += pairSums.get(i);
-            maxScore += pairSums.get(pairSums.size() - 1 - i);
-        }
-
-        return maxScore - minScore;
+        return answer;
     }
 }
