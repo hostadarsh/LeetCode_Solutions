ea78d9eee7a88068d03c50831150c9a1cee8553e
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-04-03 23:22:58 +0530

    Time: 2 ms (97.85%) | Memory: 57.1 MB (87.10%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
@@ -26,20 +26,34 @@
 //     }
 // }
 
+// class Solution {
+//     public long maximumTripletValue(int[] nums) {
+//         int maxi = Integer.MIN_VALUE;
+//         int diff = 0;
+//         long res = 0;
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             maxi = Math.max(maxi, nums[i]);
+//             if (i >= 2)
+//                 res = Math.max(res, (long)diff * nums[i]);
+//             if (i >= 1)
+//                 diff = Math.max(diff, maxi - nums[i]);
+//         }
+        
+//         return res;
+//     }
+// }
+
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        int maxi = Integer.MIN_VALUE;
-        int diff = 0;
-        long res = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            maxi = Math.max(maxi, nums[i]);
-            if (i >= 2)
-                res = Math.max(res, (long)diff * nums[i]);
-            if (i >= 1)
-                diff = Math.max(diff, maxi - nums[i]);
+        long maxProduct=0;
+        int maxDiff=0;
+        int maxNum=0;
+        for(int num: nums){
+            maxProduct = Math.max(maxProduct, (long)maxDiff*num);
+            maxNum = Math.max(maxNum, num);
+            maxDiff = Math.max(maxDiff, maxNum-num);
         }
-        
-        return res;
+        return maxProduct>0?maxProduct:0;
     }
 }
