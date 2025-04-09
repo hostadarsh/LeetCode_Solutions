1d3a74aa54bc6b5ca32fdc9f287a58adcf93f7b4
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-04-09 22:06:36 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (43.73%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,16 +1,37 @@
+// class Solution {
+//     public int minOperations(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         int mini = nums[0];
+//         if (mini < k) return -1;
+
+//         int cnt = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] > k) {
+//                 cnt++;
+//                 while (i + 1 < nums.length && nums[i] == nums[i + 1]) 
+//                     i++;
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
+
 class Solution {
     public int minOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        int mini = nums[0];
-        if (mini < k) return -1;
+        boolean[] has = new boolean[101];
 
+        for (int num : nums) {
+            has[num] = true;
+        }
         int cnt = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > k) {
+        for (int i = 0; i < has.length; i++) {
+            if (!has[i]) continue;
+
+            if (has[i] && i < k)
+                return -1;
+            else if (has[i] && i > k)
                 cnt++;
-                while (i + 1 < nums.length && nums[i] == nums[i + 1]) 
-                    i++;
-            }
         }
         return cnt;
     }
