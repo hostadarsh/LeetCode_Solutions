4517079766262f7415e9dca8e4c18ee423d5dc0a
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-04-17 02:28:38 +0530

    Time: 9 ms (94.74%) | Memory: 57.1 MB (38.09%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -35,30 +35,90 @@
 // }
 
 
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+
+//         List<Integer> arr = new ArrayList<>();
+
+//         ListNode temp = head;
+
+//         while(temp != null){
+//             arr.add(temp.val);
+//             temp = temp.next;
+//         }
+
+//         Collections.sort(arr);
+
+//         temp = head;
+//         int i = 0;
+//         while(temp !=  null){
+//             temp.val = arr.get(i++);
+//             temp = temp.next;
+//         }
+//         return head; 
+//     }
+// }
+
+
 class Solution {
     public ListNode sortList(ListNode head) {
 
-        List<Integer> arr = new ArrayList<>();
+        if( head == null || head.next == null) return head;
 
-        ListNode temp = head;
-
-        while(temp != null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
-
-        Collections.sort(arr);
-
-        temp = head;
-        int i = 0;
-        while(temp !=  null){
-            temp.val = arr.get(i++);
-            temp = temp.next;
-        }
-
-        return head;
-
-
+        ListNode middle = findMiddle(head);
         
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeTwoSortedLinkedList(left,right);
+
     }
+
+    public ListNode findMiddle(ListNode head){
+        if(head == null || head.next == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow; // middle node
+    }
+
+public ListNode mergeTwoSortedLinkedList(ListNode list1, ListNode list2){
+
+    ListNode dummyNode = new ListNode(0);
+    ListNode temp = dummyNode;
+
+    while(list1 != null && list2 != null){
+        if(list1.val <= list2.val){
+            temp.next = list1;
+            list1 = list1.next;
+        }
+        else{
+            temp.next = list2;
+            list2 = list2.next;
+        }
+        temp = temp.next;
+    }
+
+    if(list1 != null){
+        temp.next = list1;
+    }
+    else{
+        temp.next = list2;
+    }
+
+    return dummyNode.next;
+
+}
+
+
 }
