dbf164c5baf70f11966b986144ac42012ab68ff1
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-04-17 19:17:46 +0530

    Time: 26 ms (31.20%) | Memory: 45.3 MB (35.91%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,41 +1,118 @@
-class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
 
-        int max = Integer.MIN_VALUE;
+//         int max = Integer.MIN_VALUE;
         
-            for(int num : piles){
-                max = Math.max(max,num);
-            }
+//             for(int num : piles){
+//                 max = Math.max(max,num);
+//             }
           
         
 
-        int low = 1;
-        int high = max;
+//         int low = 1;
+//         int high = max;
+
+//         while(low <= high){
+//             int mid = (low + high) / 2;
+
+//             int totalH = calculateTotalHours(piles,mid);
+
+//             if(totalH <= h){
+//                 high = mid - 1;
+//             }
+//             else{
+//                 low = mid + 1;
+//             }
+//         }
+//         return low;        
+//     }
+    
+//     public static int calculateTotalHours(int[] arr, int hourly){
+//         int totalH = 0;
+//         int n = arr.length;
+
+//         for(int i = 0 ; i < n; i++){
+//             totalH += Math.ceil((double)(arr[i])/(double)(hourly));
+//         }
+//         return totalH;
+//     }
+
+
+// }
+
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
+//         int max = Max(piles);
+
+//         for(int i = 1; i <= max; i++){
+//             int time = totalHour(piles, i);
+//             if(time <= h){
+//                 return i;
+//             }
+//         }
+//         return 0;
+//     }
+
+//     public static int Max(int[] arr){
+//         int n = arr.length;
+//         int max = Integer.MIN_VALUE;
+//         for(int i = 0; i < n; i++){
+//           max = Math.max(max, arr[i]);
+//         }
+//         return max;
+//     }
+
+//     public static int totalHour(int[] arr, int hourly){
+
+//         int totalH = 0;
+
+//         for(int i = 0; i < arr.length; i++){
+//             totalH += Math.ceil( (double)(arr[i]) / (double)(hourly));
+//         }
+
+//         return totalH;
+
+//     }
+// }
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 0;
+        int high = Max(piles);
 
         while(low <= high){
+
             int mid = (low + high) / 2;
-
-            int totalH = calculateTotalHours(piles,mid);
-
-            if(totalH <= h){
+            
+            if(totalHour(piles,mid) <= h){
                 high = mid - 1;
             }
-            else{
+            else {
                 low = mid + 1;
             }
         }
-        return low;        
+        return low;
+           
     }
-    
-    public static int calculateTotalHours(int[] arr, int hourly){
-        int totalH = 0;
+
+    public static int Max(int[] arr){
         int n = arr.length;
-
-        for(int i = 0 ; i < n; i++){
-            totalH += Math.ceil((double)(arr[i])/(double)(hourly));
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+          max = Math.max(max, arr[i]);
         }
-        return totalH;
+        return max;
     }
 
+    public static int totalHour(int[] arr, int hourly){
 
+        int totalH = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            totalH += Math.ceil( (double)(arr[i]) / (double)(hourly));
+        }
+
+        return totalH;
+
+    }
 }
