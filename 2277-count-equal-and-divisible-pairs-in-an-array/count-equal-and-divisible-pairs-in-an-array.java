42a4a2387cd209da19b0f4ba8f4850f239bf02e2
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-04-17 20:43:25 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (90.53%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
@@ -1,14 +1,33 @@
+// class Solution {
+//     public int countPairs(int[] nums, int k) {
+//         int ans = 0;
+//         for(int i = 0; i < nums.length; i++){
+//             for(int j = i + 1; j < nums.length; j++){
+//                 if(nums[i] == nums[j] && i * j % k == 0){
+//                     ans++;
+//                 }
+//             }
+//         }
+//         return ans;
+        
+//     }
+// }
+
 class Solution {
     public int countPairs(int[] nums, int k) {
-        int ans = 0;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] == nums[j] && i * j % k == 0){
-                    ans++;
-                }
-            }
-        }
-        return ans;
-        
+        return helper(nums,k,0);
     }
+
+private static int helper(int[] nums, int k, int i){
+    if(i>=nums.length){
+        return 0;
+    }
+    int count = 0;
+    for(int j=i+1;j<nums.length;j++){
+        if(nums[i]==nums[j] && (i*j)%k==0){
+            count++;
+        }
+    }
+    return count+helper(nums,k,i+1);
+  }
 }
