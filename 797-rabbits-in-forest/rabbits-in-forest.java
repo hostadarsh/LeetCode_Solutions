a82a6f49ab2c46bd2d123ca338f66581a8aa9781
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-04-20 16:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (93.98%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,12 +1,30 @@
+// class Solution {
+//     public int numRabbits(int[] answers) {
+//         Map<Integer, Integer> mpp = new HashMap<>();
+//         for (int i : answers) mpp.put(i, mpp.getOrDefault(i, 0) + 1);
+
+//         int total = 0;
+//         for (Map.Entry<Integer, Integer> p : mpp.entrySet())
+//             total += Math.ceil((double)p.getValue() / (p.getKey() + 1)) * (p.getKey() + 1);
+
+//         return total;
+//     }
+// }
+
 class Solution {
     public int numRabbits(int[] answers) {
-        Map<Integer, Integer> mpp = new HashMap<>();
-        for (int i : answers) mpp.put(i, mpp.getOrDefault(i, 0) + 1);
 
-        int total = 0;
-        for (Map.Entry<Integer, Integer> p : mpp.entrySet())
-            total += Math.ceil((double)p.getValue() / (p.getKey() + 1)) * (p.getKey() + 1);
-
-        return total;
+        int n = answers.length;
+        int[] count = new int[1000];
+        
+        int ans = 0;
+        for(int x:answers){
+            if(++count[x] == 1)
+                ans += x + 1;
+            if(count[x] == x + 1)
+                count[x] = 0;
+        }
+        
+        return ans;
     }
 }
