d9df52512a37686cd4e4f3f1623d80eaa8d9d9e6
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-04-25 00:36:40 +0530

    Time: 3 ms (99.76%) | Memory: 44.7 MB (96.44%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,23 +1,49 @@
-public class Solution {
-    public int countCompleteSubarrays(int[] nums) {
-        Set<Integer> distinctElements = new HashSet<>();
-        for (int num : nums) {
-            distinctElements.add(num);
-        }
-        int totalDistinct = distinctElements.size();
-        int count = 0;
-        int n = nums.length;
+// public class Solution {
+//     public int countCompleteSubarrays(int[] nums) {
+//         Set<Integer> distinctElements = new HashSet<>();
+//         for (int num : nums) {
+//             distinctElements.add(num);
+//         }
+//         int totalDistinct = distinctElements.size();
+//         int count = 0;
+//         int n = nums.length;
         
-        for (int i = 0; i < n; i++) {
-            Set<Integer> currentSet = new HashSet<>();
-            for (int j = i; j < n; j++) {
-                currentSet.add(nums[j]);
-                if (currentSet.size() == totalDistinct) {
-                    count += (n - j);
-                    break;
-                }
+//         for (int i = 0; i < n; i++) {
+//             Set<Integer> currentSet = new HashSet<>();
+//             for (int j = i; j < n; j++) {
+//                 currentSet.add(nums[j]);
+//                 if (currentSet.size() == totalDistinct) {
+//                     count += (n - j);
+//                     break;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        boolean[] exists = new boolean[2001];
+        int distinct = 0;
+        for( int n : nums ){
+            if( !exists[n] ){
+                exists[n] = true;
+                distinct++;
             }
         }
-        return count;
+        int[] freq = new int[2001];
+        int count = 0, n = nums.length;
+        int sub = 0;
+        for( int start = 0, end = 0; end < n; end++ ){
+            if( freq[ nums[ end ] ]++ == 0 )
+                count++;
+            while( count == distinct ){
+                sub += n - end;
+                if( freq[ nums[ start ++ ] ]-- == 1 )
+                    count--;
+            }
+        }
+        return sub;
     }
 }
