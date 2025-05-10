62fff5bfe92ca9e178a073130a172bc9932ba6cf
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-05-10 18:45:11 +0530

    Time: 1 ms (100.00%) | Memory: 60.8 MB (47.42%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -1,40 +1,73 @@
+// class Solution {
+//     public long minSum(int[] nums1, int[] nums2) {
+//         long sum1 = sum(nums1);
+//         long sum2 = sum(nums2);
+
+//         long count1 = count(nums1);
+//         long count2 = count(nums2);
+
+//         if(count1 == 0 && count2 == 0){
+//             return sum1 == sum2 ? sum1 : -1;
+//         }
+
+
+//         if(sum1 + count1 >= sum2 + count2 && count2 != 0){
+//             return sum1 + count1;
+//         }   
+//         if(sum1 + count1 <= sum2 + count2 && count1 != 0 ){
+//             return sum2 + count2;
+//         } 
+//         return -1;    
+        
+//     }
+
+//     public static long sum(int[] arr){
+//         long sum = 0;
+//         for(int i = 0; i < arr.length; i++){
+//             sum = sum + arr[i];
+//         }
+//         return sum;
+//     }
+//     public static long count(int[] arr){
+//         long count = 0;
+//         for(int i = 0; i < arr.length; i++){
+//             if(arr[i] == 0){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public long minSum(int[] nums1, int[] nums2) {
-        long sum1 = sum(nums1);
-        long sum2 = sum(nums2);
+        long sum1 = 0;
+        long zeroCount1 = 0;
 
-        long count1 = count(nums1);
-        long count2 = count(nums2);
-
-        if(count1 == 0 && count2 == 0){
-            return sum1 == sum2 ? sum1 : -1;
+        for (int n: nums1) {
+            sum1 += (long) n;
+            zeroCount1 += n == 0 ? 1 : 0;
         }
 
+        long sum2 = 0;
+        long zeroCount2 = 0;
 
-        if(sum1 + count1 >= sum2 + count2 && count2 != 0){
-            return sum1 + count1;
-        }   
-        if(sum1 + count1 <= sum2 + count2 && count1 != 0 ){
-            return sum2 + count2;
-        } 
-        return -1;    
-        
-    }
+        for (int n: nums2) {
+            sum2 += (long) n;
+            zeroCount2 += n == 0 ? 1 : 0;
+        }
 
-    public static long sum(int[] arr){
-        long sum = 0;
-        for(int i = 0; i < arr.length; i++){
-            sum = sum + arr[i];
+        long minSum1 = sum1+zeroCount1;
+        long minSum2 = sum2+zeroCount2;
+
+        if (minSum1<minSum2 && zeroCount1 == 0) {
+            return -1;
         }
-        return sum;
-    }
-    public static long count(int[] arr){
-        long count = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 0){
-                count++;
-            }
+
+        if (minSum1>minSum2 && zeroCount2 == 0) {
+            return -1;
         }
-        return count;
+
+        return Math.max(minSum1, minSum2);
     }
 }
