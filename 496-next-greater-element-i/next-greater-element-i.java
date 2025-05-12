b7b3bb890fad69e56f99d10037f631da490febac
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-05-12 19:30:15 +0530

    Time: 3 ms (90.34%) | Memory: 44.4 MB (31.61%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,60 @@
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         int[] ans = new int[nums1.length];
+
+//         for(int i = 0; i < nums1.length; i++){
+//             int index = -1;
+
+//             for(int j = 0; j < nums2.length; j++){
+//                 if(nums1[i] == nums2[j]){
+//                     index = j;
+//                     break;
+//                 }
+//             }
+
+//             int nextGreaterElement = -1;
+//             for(int k = index + 1; k < nums2.length; k++){
+//                 if( nums2[k] > nums1[i] ){
+//                     nextGreaterElement = nums2[k];
+//                     break;
+//                 }
+//             }
+//             ans[i] = nextGreaterElement;
+//         }
+
+//         return ans;
+
+//     }
+// }
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
         int[] ans = new int[nums1.length];
+        
+        Stack<Integer> st = new Stack<>(); 
+        st.push(nums2[nums2.length-1]);
 
-        for(int i = 0; i < nums1.length; i++){
-            int index = -1;
+        for(int i = nums2.length-1; i >= 0; i--){
 
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    index = j;
-                    break;
-                }
+            while(!st.isEmpty() && st.peek() <= nums2[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                mp.put(nums2[i], -1);
+            }
+            else{
+                mp.put(nums2[i], st.peek());
             }
 
-            int nextGreaterElement = -1;
-            for(int k = index + 1; k < nums2.length; k++){
-                if( nums2[k] > nums1[i] ){
-                    nextGreaterElement = nums2[k];
-                    break;
-                }
-            }
-            ans[i] = nextGreaterElement;
+            st.push(nums2[i]);
         }
 
+        for(int i = 0; i < nums1.length; i++){
+            ans[i] = mp.get(nums1[i]);
+        }
         return ans;
-
+        
     }
 }
