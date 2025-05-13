0e7ae2365c256365c92629ee319278e1696bbf33
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-05-13 13:59:22 +0530

    Time: 69 ms (61.47%) | Memory: 57.7 MB (35.21%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,53 +1,81 @@
+// class Solution {
+//     public int largestRectangleArea(int[] heights) {
+
+//         int[] rb = new int[heights.length];
+       
+//         Stack<Integer> st = new Stack<>();
+//         st.push(heights.length-1);
+//         rb[heights.length - 1] = heights.length;
+
+//         for(int i = heights.length - 2; i >= 0; i--){
+//             while(!st.isEmpty() && heights[st.peek()] >= heights[i] ){
+//                 st.pop();
+//             }
+//             if(st.isEmpty()){
+//                 rb[i] = heights.length;
+//             }
+//             else{
+//                 rb[i] = st.peek();
+//             }
+//             st.push(i);
+//         }
+
+//         int[] lb = new int[heights.length];
+//         st = new Stack<>();
+//         st.push(0);
+//         lb[0] = -1; 
+
+//         for(int i = 0; i < heights.length; i++){
+//             while(!st.isEmpty() && heights[st.peek()] >= heights[i]){
+//                 st.pop();
+//             }
+
+//             if(st.isEmpty()){
+//                 lb[i] = -1;
+//             }
+//             else{
+//                 lb[i] = st.peek();
+//             }
+//             st.push(i);
+//         }
+
+//         int maxArea = 0;
+
+//         for(int i = 0; i < heights.length; i++){
+//             int area = (rb[i] - lb[i] - 1) * heights[i];
+//             maxArea = Math.max(maxArea, area);
+//         }
+
+//         return maxArea;
+
+        
+//     }
+// }
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
 
-        int[] rb = new int[heights.length];
-       
+        int n = heights.length;
         Stack<Integer> st = new Stack<>();
-        st.push(heights.length-1);
-        rb[heights.length - 1] = heights.length;
+        int maxA = 0;
 
-        for(int i = heights.length - 2; i >= 0; i--){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i] ){
+        for(int i = 0; i <= n; i++){
+            while(!st.isEmpty() && (i == n || heights[st.peek()] >= heights[i])){
+                int height = heights[st.peek()];
                 st.pop();
-            }
-            if(st.isEmpty()){
-                rb[i] = heights.length;
-            }
-            else{
-                rb[i] = st.peek();
+                int width;
+
+                if(st.isEmpty()){
+                    width = i;
+                }
+                else{
+                    width = i - st.peek() - 1;
+                    }
+                maxA = Math.max(maxA, width * height);
             }
             st.push(i);
         }
+        return maxA;
 
-        int[] lb = new int[heights.length];
-        st = new Stack<>();
-        st.push(0);
-        lb[0] = -1; 
-
-        for(int i = 0; i < heights.length; i++){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i]){
-                st.pop();
-            }
-
-            if(st.isEmpty()){
-                lb[i] = -1;
-            }
-            else{
-                lb[i] = st.peek();
-            }
-            st.push(i);
-        }
-
-        int maxArea = 0;
-
-        for(int i = 0; i < heights.length; i++){
-            int area = (rb[i] - lb[i] - 1) * heights[i];
-            maxArea = Math.max(maxArea, area);
-        }
-
-        return maxArea;
-
-        
     }
 }
