2c4ffc9ca89fe06bf957af7c591e16f11f13a9ce
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-05-14 08:56:45 +0530

    Time: 69 ms (61.47%) | Memory: 58 MB (22.03%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -52,30 +52,61 @@
 //     }
 // }
 
+// class Solution {
+//     public int largestRectangleArea(int[] heights) {
+
+//         int n = heights.length;
+//         Stack<Integer> st = new Stack<>();
+//         int maxA = 0;
+
+//         for(int i = 0; i <= n; i++){
+//             while(!st.isEmpty() && (i == n || heights[st.peek()] >= heights[i])){
+//                 int height = heights[st.peek()];
+//                 st.pop();
+//                 int width;
+
+//                 if(st.isEmpty()){
+//                     width = i;
+//                 }
+//                 else{
+//                     width = i - st.peek() - 1;
+//                     }
+//                 maxA = Math.max(maxA, width * height);
+//             }
+//             st.push(i);
+//         }
+//         return maxA;
+
+//     }
+// }
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
 
-        int n = heights.length;
         Stack<Integer> st = new Stack<>();
+        int n = heights.length;
         int maxA = 0;
 
-        for(int i = 0; i <= n; i++){
-            while(!st.isEmpty() && (i == n || heights[st.peek()] >= heights[i])){
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && heights[i] <= heights[st.peek()]){
                 int height = heights[st.peek()];
                 st.pop();
                 int width;
-
                 if(st.isEmpty()){
                     width = i;
                 }
                 else{
                     width = i - st.peek() - 1;
-                    }
-                maxA = Math.max(maxA, width * height);
+                }
+                maxA = Math.max(maxA, height * width); 
             }
             st.push(i);
         }
+         while(!st.isEmpty()){
+            int height = heights[st.pop()];
+            int width = st.isEmpty() ? n : n - st.peek() - 1;
+            maxA = Math.max(maxA, height * width);
+        }
         return maxA;
-
     }
 }
