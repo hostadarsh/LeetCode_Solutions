df19c4bb1373033386aec090fb3c98a83cfba59c
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-05-20 01:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (75.16%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,14 +1,37 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public String triangleType(int[] nums) {
+//         Arrays.sort(nums);
+//         Set<Integer> set = new HashSet<>();
+//         for (int num : nums) set.add(num);
+
+//         if (nums[0] + nums[1] <= nums[2]) return "none";
+//         if (set.size() == 1) return "equilateral";
+//         if (set.size() == 2) return "isosceles";
+//         return "scalene";
+//     }
+// }
 
 class Solution {
     public String triangleType(int[] nums) {
-        Arrays.sort(nums);
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) set.add(num);
+        if (nums == null || nums.length != 3) {
+            return "none";
+        }
+        int a = nums[0], b = nums[1], c = nums[2];
 
-        if (nums[0] + nums[1] <= nums[2]) return "none";
-        if (set.size() == 1) return "equilateral";
-        if (set.size() == 2) return "isosceles";
-        return "scalene";
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return "none";
+        }
+
+
+        if (a == b && b == c) {
+            return "equilateral";
+        }
+        if (a == b || b == c || c == a) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
     }
 }
