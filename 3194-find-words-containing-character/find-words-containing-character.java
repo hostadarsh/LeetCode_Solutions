02c0f64c536b830f3d457e9382ed4dc54f14ce5a
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-05-25 00:13:39 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (59.58%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.java
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -1,29 +1,42 @@
+// class Solution {
+//     public List<Integer> findWordsContaining(String[] words, char x) {
+//         List<List<Character>> charLists = new ArrayList<>();
+
+//         List<Integer> ans = new ArrayList<>();
+//         int n = 0;
+
+//         for(String s : words){
+//             List<Character> charList = new ArrayList<>();
+//             for(char c : s.toCharArray()){
+//                 charList.add(c);
+//             }
+//             charLists.add(charList);
+//         }
+
+//         for(List<Character> list : charLists){
+//             if(list.contains(x)){
+//                 ans.add(n);
+//                 n++;
+//             }
+//             else{
+//                 n++;
+//             }
+//         }
+
+//         return ans;
+
+//     }
+// }
+
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
-        List<List<Character>> charLists = new ArrayList<>();
-
-        List<Integer> ans = new ArrayList<>();
-        int n = 0;
-
-        for(String s : words){
-            List<Character> charList = new ArrayList<>();
-            for(char c : s.toCharArray()){
-                charList.add(c);
-            }
-            charLists.add(charList);
-        }
-
-        for(List<Character> list : charLists){
-            if(list.contains(x)){
-                ans.add(n);
-                n++;
-            }
-            else{
-                n++;
+        List<Integer> resultIndices = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                resultIndices.add(i);
             }
         }
-
-        return ans;
-
+        return resultIndices;
+        
     }
 }
