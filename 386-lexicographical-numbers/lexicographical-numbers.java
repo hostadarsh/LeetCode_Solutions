14c583b92b7650e4556cfa71430283c7e5fb7080
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-06-08 20:48:31 +0530

    Time: 5 ms (56.53%) | Memory: 48.3 MB (69.78%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,3 +1,27 @@
+// import java.util.*;
+
+// class Solution {
+//     public List<Integer> lexicalOrder(int n) {
+//         List<Integer> result = new ArrayList<>();
+//         int count = 1;
+
+//         for (int i = 0; i < n; i++) {
+//             result.add(count);
+//             if (count * 10 <= n) {
+//                 count *= 10;  // Move to next lexicographical level
+//             } else {
+//                 if (count >= n) count /= 10;  // Backtrack if needed
+//                 count++;
+//                 while (count % 10 == 0) {
+//                     count /= 10;  // Skip trailing zeros
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
@@ -5,19 +29,24 @@ class Solution {
         List<Integer> result = new ArrayList<>();
         int count = 1;
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n ; i++){
             result.add(count);
-            if (count * 10 <= n) {
-                count *= 10;  // Move to next lexicographical level
-            } else {
-                if (count >= n) count /= 10;  // Backtrack if needed
+
+            if(count * 10 <= n){
+                count = count * 10;
+            }
+            else{
+                if(count >= n){
+                    count = count / 10;
+                }
                 count++;
-                while (count % 10 == 0) {
-                    count /= 10;  // Skip trailing zeros
+
+                while(count % 10 == 0){
+                    count = count / 10;
                 }
             }
         }
-
         return result;
     }
 }
+
