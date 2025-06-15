d5b4b70f42e852c5401369c58d67d01f450c998d
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-06-16 01:19:16 +0530

    Time: 2 ms (27.74%) | Memory: 40.8 MB (50.36%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -1,24 +1,43 @@
-import java.util.*;
+// import java.util.*;
 
-public class Solution {
+// public class Solution {
+//     public int maxDiff(int num) {
+//         String numStr = String.valueOf(num);
+//         Set<Character> uniqueDigits = new HashSet<>();
+//         for (char ch : numStr.toCharArray()) {
+//             uniqueDigits.add(ch);
+//         }
+
+//         int maxVal = num, minVal = num;
+
+//         for (char digit : uniqueDigits) {
+//             for (char newDigit = '0'; newDigit <= '9'; newDigit++) {
+//                 if (numStr.charAt(0) == digit && newDigit == '0') continue;
+//                 String newNumStr = numStr.replace(digit, newDigit);
+//                 int newNum = Integer.parseInt(newNumStr);
+//                 maxVal = Math.max(maxVal, newNum);
+//                 minVal = Math.min(minVal, newNum);
+//             }
+//         }
+//         return maxVal - minVal;
+//     }
+// }
+
+class Solution {
     public int maxDiff(int num) {
-        String numStr = String.valueOf(num);
-        Set<Character> uniqueDigits = new HashSet<>();
-        for (char ch : numStr.toCharArray()) {
-            uniqueDigits.add(ch);
+        int max = num, min=num;
+        String s = String.valueOf(num);
+        char first = s.charAt(0);
+        for(char ch:s.toCharArray()){
+            String maxs = s.replace(ch, '9');
+            String mins = s.replace(ch, '0');
+            if(ch==first)mins=s.replace(ch, '1');
+            int tmax = Integer.valueOf(maxs);
+            int tmin = Integer.valueOf(mins);
+            max = Math.max(max, tmax);
+            if(tmin!=0)min = Math.min(min, tmin);
         }
 
-        int maxVal = num, minVal = num;
-
-        for (char digit : uniqueDigits) {
-            for (char newDigit = '0'; newDigit <= '9'; newDigit++) {
-                if (numStr.charAt(0) == digit && newDigit == '0') continue;
-                String newNumStr = numStr.replace(digit, newDigit);
-                int newNum = Integer.parseInt(newNumStr);
-                maxVal = Math.max(maxVal, newNum);
-                minVal = Math.min(minVal, newNum);
-            }
-        }
-        return maxVal - minVal;
+        return max-min;
     }
 }
