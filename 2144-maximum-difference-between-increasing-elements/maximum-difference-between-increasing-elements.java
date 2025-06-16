d5505ff5092bbbd41cdaa3d300a1d527c7dd35de
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-06-17 00:23:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (8.09%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,18 +1,41 @@
+// class Solution {
+//     public int maximumDifference(int[] nums) {
+//         Stack<int[]> stack = new Stack<>(); // [value, index]
+//         int diff = -1;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (stack.isEmpty() || nums[i] < stack.peek()[0]) {
+//                 stack.push(new int[]{nums[i], i});
+//             }
+
+//             if (!stack.isEmpty() && stack.peek()[1] < i && nums[i] > stack.peek()[0]) {
+//                 diff = Math.max(diff, nums[i] - stack.peek()[0]);
+//             }
+//         }
+
+//         return diff;
+//     }
+// }
+
+
 class Solution {
     public int maximumDifference(int[] nums) {
-        Stack<int[]> stack = new Stack<>(); // [value, index]
-        int diff = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (stack.isEmpty() || nums[i] < stack.peek()[0]) {
-                stack.push(new int[]{nums[i], i});
+        int i = 0;
+        int max = 0;
+        for(int j = 1; j < nums.length; j++){
+            if(j > i && nums[j] > nums[i]){
+                int tempMax = nums[j] - nums[i];
+                if(tempMax > max){
+                    max = tempMax;
+                }
             }
-
-            if (!stack.isEmpty() && stack.peek()[1] < i && nums[i] > stack.peek()[0]) {
-                diff = Math.max(diff, nums[i] - stack.peek()[0]);
+            else {
+                i = j;
             }
         }
-
-        return diff;
+        if(max == 0){
+            return -1;
+        }
+        return max;
     }
 }
