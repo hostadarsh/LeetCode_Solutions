e32c8d97cadb699c6022076e0855b13faa5bfab9
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-06-19 00:48:35 +0530

    Time: 27 ms (9.95%) | Memory: 59.1 MB (86.39%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -0,0 +1,66 @@
+// class Solution {
+//     public List<List<Integer>> divideArray(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         List<List<Integer>> ans = new ArrayList<>();
+
+//         for (int i = 0; i < nums.length; i += 3) {
+//             if ( nums[i + 2] - nums[i] > k) {
+//                 return new ArrayList<>();
+//             }
+//             ans.add(Arrays.asList(nums[i], nums[i + 1], nums[i + 2]));
+//         }
+
+//         return ans;
+//     }
+// }
+
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        List<List<Integer>> temp = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][];
+            }
+            temp.add(Arrays.asList(nums[i], nums[i + 1], nums[i + 2]));
+        }
+
+        // Convert List<List<Integer>> to int[][]
+        int[][] result = new int[temp.size()][3];
+        for (int i = 0; i < temp.size(); i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = temp.get(i).get(j);
+            }
+        }
+
+        return result;
+    }
+}
+
+
+// class Solution {
+//     public List<List<Integer>> divideArray(int[] nums, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         for (int num : nums) {
+//             pq.offer(num);
+//         }
+
+//         List<List<Integer>> res = new ArrayList<>();
+
+//         while (pq.size() >= 3) {
+//             int low = pq.poll();
+//             int mid = pq.poll();
+//             int high = pq.poll();
+
+//             if (high - low <= k) {
+//                 res.add(Arrays.asList(low, mid, high));
+//             } else {
+//                 return new ArrayList<>();
+//             }
+//         }
+
+//         return res;
+//     }
+// }
+
