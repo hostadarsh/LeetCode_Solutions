3f8df4edf919902ec59c583625d870ff0e809372
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-06-21 01:56:07 +0530

    Time: 47 ms (100.00%) | Memory: 45.9 MB (28.95%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -1,38 +1,69 @@
+// class Solution {
+//     private void compute(int dir1, int dir2, int dir3, int dir4, int[] k_cnt, int[] dist) {
+//         dist[dir1] += (k_cnt[dir1] > 0) ? 1 : -1;
+//         k_cnt[dir1]--;
+
+//         dist[dir2] += (k_cnt[dir2] > 0) ? 1 : -1;
+//         k_cnt[dir2]--;
+
+//         dist[dir3]++;
+//         dist[dir4]++;
+//     }
+
+//     public int maxDistance(String s, int k) {
+//         if (s.length() == k) return k;
+
+//         int mx = 0;
+//         int[] k_cnt = new int[4];
+//         int[] dist = new int[4];
+//         Arrays.fill(k_cnt, k);
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             if (ch == 'N') {
+//                 compute(1, 3, 0, 2, k_cnt, dist);
+//             } else if (ch == 'E') {
+//                 compute(2, 3, 1, 0, k_cnt, dist);
+//             } else if (ch == 'W') {
+//                 compute(0, 1, 2, 3, k_cnt, dist);
+//             } else { // 'S'
+//                 compute(0, 2, 1, 3, k_cnt, dist);
+//             }
+
+//             mx = Math.max(mx, Math.max(Math.max(dist[0], dist[1]), Math.max(dist[2], dist[3])));
+//         }
+
+//         return mx;
+//     }
+// }
+
 class Solution {
-    private void compute(int dir1, int dir2, int dir3, int dir4, int[] k_cnt, int[] dist) {
-        dist[dir1] += (k_cnt[dir1] > 0) ? 1 : -1;
-        k_cnt[dir1]--;
+    public int maxDistance(String st, int k) {
+        int n = 0;
+        int s = 0;
+        int e = 0;
+        int w = 0;
 
-        dist[dir2] += (k_cnt[dir2] > 0) ? 1 : -1;
-        k_cnt[dir2]--;
+        int result = 0;
+        char[] chars = st.toCharArray();
 
-        dist[dir3]++;
-        dist[dir4]++;
-    }
-
-    public int maxDistance(String s, int k) {
-        if (s.length() == k) return k;
-
-        int mx = 0;
-        int[] k_cnt = new int[4];
-        int[] dist = new int[4];
-        Arrays.fill(k_cnt, k);
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == 'N') {
-                compute(1, 3, 0, 2, k_cnt, dist);
-            } else if (ch == 'E') {
-                compute(2, 3, 1, 0, k_cnt, dist);
-            } else if (ch == 'W') {
-                compute(0, 1, 2, 3, k_cnt, dist);
-            } else { // 'S'
-                compute(0, 2, 1, 3, k_cnt, dist);
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == 'N') {
+                n++;
+            } else if (c == 'S') {
+                s++;
+            } else if (c == 'E') {
+                e++;
+            } else {
+                w++;
             }
 
-            mx = Math.max(mx, Math.max(Math.max(dist[0], dist[1]), Math.max(dist[2], dist[3])));
+            int current = Math.abs(n-s) + Math.abs(e-w);
+            int distance = current + Math.min(2*k,i+1-current);
+            result = Math.max(result, distance);
         }
 
-        return mx;
+        return result;
     }
 }
