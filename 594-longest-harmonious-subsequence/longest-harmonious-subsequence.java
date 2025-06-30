db205c854875f5423c185e9836b270dee5969213
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-07-01 02:46:25 +0530

    Time: 16 ms (62.38%) | Memory: 45.7 MB (60.22%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -1,15 +1,37 @@
+// public class Solution {
+//     public int findLHS(int[] nums) {
+//         Arrays.sort(nums);
+//         int j = 0;
+//         int maxLength = 0;
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             while (nums[i] - nums[j] > 1) {
+//                 j++;
+//             }
+//             if (nums[i] - nums[j] == 1) {
+//                 maxLength = Math.max(maxLength, i - j + 1);
+//             }
+//         }
+//         return maxLength;
+//     }
+// }
+
 public class Solution {
     public int findLHS(int[] nums) {
-        Arrays.sort(nums);
-        int j = 0;
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        
+        // Step 1: Count frequencies of each number
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+        
         int maxLength = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] - nums[j] > 1) {
-                j++;
-            }
-            if (nums[i] - nums[j] == 1) {
-                maxLength = Math.max(maxLength, i - j + 1);
+        // Step 2: Check all possible (num, num+1) pairs
+        for (int num : frequencyMap.keySet()) {
+            if (frequencyMap.containsKey(num + 1)) {
+                int currentLength = frequencyMap.get(num) + frequencyMap.get(num + 1);
+                maxLength = Math.max(maxLength, currentLength);
             }
         }
         return maxLength;
