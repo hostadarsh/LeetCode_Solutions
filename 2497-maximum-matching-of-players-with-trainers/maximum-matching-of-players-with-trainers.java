1a82ea2188a5bfe126d1d68f60f1893b62ee3fd0
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2025-07-14 00:50:30 +0530

    Time: 7 ms (98.94%) | Memory: 64.5 MB (34.97%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,17 +1,47 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+//         Arrays.sort(players);
+//         Arrays.sort(trainers);
+//         int i = 0, j = 0, ans = 0;
+//         while (i < players.length && j < trainers.length) {
+//             if (players[i] <= trainers[j]) {  // can pair
+//                 ++ans;
+//                 ++i;
+//             }
+//             ++j;   // trainer consumed
+//         }
+//         return ans;
+//     }
+// }
 
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Arrays.sort(players);
-        Arrays.sort(trainers);
-        int i = 0, j = 0, ans = 0;
-        while (i < players.length && j < trainers.length) {
-            if (players[i] <= trainers[j]) {  // can pair
-                ++ans;
-                ++i;
-            }
-            ++j;   // trainer consumed
+        int m = players.length;
+        int n = trainers.length;
+
+        Thread t1 = new Thread(() -> Arrays.sort(players));
+        Thread t2 = new Thread(() -> Arrays.sort(trainers));
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
         }
-        return ans;
+
+        int left = 0;
+        int right = 0;
+        int count = 0;
+
+        while (left < m && right < n) {
+            if (trainers[right] >= players[left]) {
+                count++;
+                left++;
+            }
+            right++;
+        }
+        return count;
     }
 }
