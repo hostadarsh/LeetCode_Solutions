8af74b2c967aefa828aba7f892a438eaf17e4e80
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-03-18 18:35:14 +0530

    Time: 2 ms (100.00%) | Memory: 161.1 MB (85.48%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,25 +1,99 @@
+// class Solution {
+//     public int countSubmatrices(int[][] grid, int k) {
+//         int m = grid.length, n = grid[0].length;
+//         int ans = 0;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 long up = (i > 0 ? grid[i - 1][j] : 0);
+//                 long left = (j > 0 ? grid[i][j - 1] : 0);
+//                 long diag = (i > 0 && j > 0 ? grid[i - 1][j - 1] : 0);
+
+//                 grid[i][j] = (int)(grid[i][j] + up + left - diag);
+
+//                 if (grid[i][j] <= k) {
+//                     ans++;
+//                 } else {
+//                     if (j == 0) return ans;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+// class Solution {
+
+//     public int countSubmatrices(int[][] grid, int k) {
+//         int n = grid.length;
+//         int m = grid[0].length;
+//         int[] cols = new int[m];
+//         int res = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             int rows = 0;
+//             for (int j = 0; j < m; j++) {
+//                 cols[j] += grid[i][j];
+//                 rows += cols[j];
+//                 if (rows <= k) {
+//                     res++;
+//                 }
+//             }
+//         }
+
+//         return res;
+//     }
+// }
+
+
+
+
 class Solution {
+
     public int countSubmatrices(int[][] grid, int k) {
-        int m = grid.length, n = grid[0].length;
-        int ans = 0;
+        int m = grid[0].length;
+        int n = grid.length;
+        int res = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                long up = (i > 0 ? grid[i - 1][j] : 0);
-                long left = (j > 0 ? grid[i][j - 1] : 0);
-                long diag = (i > 0 && j > 0 ? grid[i - 1][j - 1] : 0);
+        int [] cols = new int[m];
+        
+        for(int i = 0; i < n; i++){
+            int rows = 0;
 
-                grid[i][j] = (int)(grid[i][j] + up + left - diag);
+            for(int j = 0; j < m; j++){
+                cols[j] = cols[j] + grid[i][j];
+                rows = rows + cols[j];
 
-                if (grid[i][j] <= k) {
-                    ans++;
-                } else {
-                    if (j == 0) return ans;
-                    break;
+                if(rows <= k){
+                    res++;
                 }
             }
         }
 
-        return ans;
+        return res;
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
