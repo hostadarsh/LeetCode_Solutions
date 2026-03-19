fedce9665ac1d61863529b87642313e433930f3a
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-03-19 23:31:01 +0530

    Time: 16 ms (100.00%) | Memory: 259.2 MB (23.08%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,24 +1,30 @@
 class Solution {
     public int numberOfSubmatrices(char[][] grid) {
-        int rows = grid.length, cols = grid[0].length;
-        int[] sumX = new int[cols];
-        int[] sumY = new int[cols];
-        int res = 0;
 
-        for (int i = 0; i < rows; i++) {
-            int rx = 0, ry = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] colX = new int[n];
+        int[] colY = new int[n];
+        int ans = 0;
 
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 'X') rx++;
-                else if (grid[i][j] == 'Y') ry++;
 
-                sumX[j] += rx;
-                sumY[j] += ry;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
 
-                if (sumX[j] > 0 && sumX[j] == sumY[j]) res++;
+                if(grid[i][j] == 'X') colX[j]++;
+                if(grid[i][j] == 'Y') colY[j]++;
+            }
+            
+            int x = 0, y = 0;
+            for(int j = 0; j < n; j++) {
+
+                x += colX[j];
+                y += colY[j];
+
+                if(x == y && x > 0) ans++;
             }
         }
 
-        return res;
+        return ans;
     }
 }
