a311a363e671a697b56a8753f8716b2e430e2fef
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-03-31 23:35:42 +0530

    Time: 3 ms (100.00%) | Memory: 46.7 MB (66.67%) - LeetSync

--- 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.java
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.java
@@ -1,56 +1,141 @@
+// class Solution {
+//     public String generateString(String S, String t) {
+//         char[] s = S.toCharArray();
+//         int n = s.length;
+//         int m = t.length();
+//         char[] ans = new char[n + m - 1];
+//         Arrays.fill(ans, '?'); // '?' indicates undecided positions
+
+//         // Process T
+//         for (int i = 0; i < n; i++) {
+//             if (s[i] != 'T') {
+//                 continue;
+//             }
+//             // Substring must equal t
+//             for (int j = 0; j < m; j++) {
+//                 char v = ans[i + j];
+//                 if (v != '?' && v != t.charAt(j)) {
+//                     return "";
+//                 }
+//                 ans[i + j] = t.charAt(j);
+//             }
+//         }
+
+//         char[] oldAns = ans.clone();
+//         for (int i = 0; i < ans.length; i++) {
+//             if (ans[i] == '?') {
+//                 ans[i] = 'a'; // initial value for undecided positions is 'a'
+//             }
+//         }
+
+//         // Process F
+//         for (int i = 0; i < n; i++) {
+//             if (s[i] != 'F') {
+//                 continue;
+//             }
+//             // Substring must not equal t
+//             if (!new String(ans, i, m).equals(t)) {
+//                 continue;
+//             }
+//             // Find the last undecided position
+//             boolean ok = false;
+//             for (int j = i + m - 1; j >= i; j--) {
+//                 if (oldAns[j] == '?') { // previously filled with 'a', now change to 'b'
+//                     ans[j] = 'b';
+//                     ok = true;
+//                     break;
+//                 }
+//             }
+//             if (!ok) {
+//                 return "";
+//             }
+//         }
+
+//         return new String(ans);
+//     }
+// }
+
+
+// https://www.youtube.com/@0x3f
 class Solution {
     public String generateString(String S, String t) {
         char[] s = S.toCharArray();
         int n = s.length;
         int m = t.length();
         char[] ans = new char[n + m - 1];
-        Arrays.fill(ans, '?'); // '?' indicates undecided positions
+        Arrays.fill(ans, '?');
 
-        // Process T
+        // 处理 T
+        int[] z = calcZ(t);
+        int pre = -m;
         for (int i = 0; i < n; i++) {
             if (s[i] != 'T') {
                 continue;
             }
-            // Substring must equal t
-            for (int j = 0; j < m; j++) {
-                char v = ans[i + j];
-                if (v != '?' && v != t.charAt(j)) {
-                    return "";
-                }
+            int size = Math.max(pre + m - i, 0);
+            // t 的长为 size 的前后缀必须相同
+            if (size > 0 && z[m - size] < size) {
+                return "";
+            }
+            // size 后的内容都是 '?'，填入 t
+            for (int j = size; j < m; j++) {
                 ans[i + j] = t.charAt(j);
             }
+            pre = i;
         }
 
-        char[] oldAns = ans.clone();
+        // 计算 <= i 的最近待定位置
+        int[] preQ = new int[ans.length];
+        pre = -1;
         for (int i = 0; i < ans.length; i++) {
             if (ans[i] == '?') {
-                ans[i] = 'a'; // initial value for undecided positions is 'a'
+                ans[i] = 'a'; // 待定位置的初始值为 a
+                pre = i;
             }
+            preQ[i] = pre;
         }
 
-        // Process F
+        // 找 ans 中的等于 t 的位置，可以用 KMP 或者 Z 函数
+        z = calcZ(t + new String(ans));
+
+        // 处理 F
         for (int i = 0; i < n; i++) {
             if (s[i] != 'F') {
                 continue;
             }
-            // Substring must not equal t
-            if (!new String(ans, i, m).equals(t)) {
+            // 子串必须不等于 t
+            if (z[m + i] < m) {
                 continue;
             }
-            // Find the last undecided position
-            boolean ok = false;
-            for (int j = i + m - 1; j >= i; j--) {
-                if (oldAns[j] == '?') { // previously filled with 'a', now change to 'b'
-                    ans[j] = 'b';
-                    ok = true;
-                    break;
-                }
-            }
-            if (!ok) {
+            // 找最后一个待定位置
+            int j = preQ[i + m - 1];
+            if (j < i) { // 没有
                 return "";
             }
+            ans[j] = 'b';
+            i = j; // 直接跳到 j
         }
 
         return new String(ans);
+    }
+
+    private int[] calcZ(String S) {
+        char[] s = S.toCharArray();
+        int n = s.length;
+        int[] z = new int[n];
+        int boxL = 0; // z-box 左右边界（闭区间）
+        int boxR = 0;
+        for (int i = 1; i < n; i++) {
+            if (i <= boxR) {
+                z[i] = Math.min(z[i - boxL], boxR - i + 1);
+            }
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+                boxL = i;
+                boxR = i + z[i];
+                z[i]++;
+            }
+        }
+        z[0] = n;
+        return z;
     }
 }
