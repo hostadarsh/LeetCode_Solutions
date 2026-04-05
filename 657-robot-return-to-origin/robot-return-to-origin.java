aa0c57f72944e19d4f0dc202f5775fe46f4c2372
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-04-06 00:23:00 +0530

    Time: 6 ms (49.49%) | Memory: 45.6 MB (65.59%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,9 +1,43 @@
+// class Solution {
+//     public boolean judgeCircle(String moves) {
+//         int countR = 0;
+//         int countL = 0;
+//         int countU = 0;
+//         int countD = 0;
+
+//         for(int i = 0; i < moves.length(); i++){
+//             char ch = moves.charAt(i);
+
+//         // for(char ch : moves.tocharArray()){
+
+//             if(ch == 'U'){
+//                 countU++;
+//             }
+//             if(ch == 'D'){
+//                 countD++;
+//             }
+//             if(ch == 'L'){
+//                 countL++;
+//             }
+//             if(ch == 'R'){
+//                 countR++;
+//             }
+//         }
+
+//         if(countR == countL && countU == countD){
+//             return true;
+//         }
+
+//         return false;
+//     }
+// }
+
+
+
 class Solution {
     public boolean judgeCircle(String moves) {
-        int countR = 0;
         int countL = 0;
         int countU = 0;
-        int countD = 0;
 
         for(int i = 0; i < moves.length(); i++){
             char ch = moves.charAt(i);
@@ -14,17 +48,17 @@ class Solution {
                 countU++;
             }
             if(ch == 'D'){
-                countD++;
+                countU--;
             }
             if(ch == 'L'){
                 countL++;
             }
             if(ch == 'R'){
-                countR++;
+                countL--;
             }
         }
 
-        if(countR == countL && countU == countD){
+        if(countL == 0 && countU == 0){
             return true;
         }
 
