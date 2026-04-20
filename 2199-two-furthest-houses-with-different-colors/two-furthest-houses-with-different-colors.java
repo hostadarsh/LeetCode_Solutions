289ea87db55152e21805602f952baf0590b7aefa
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-04-20 22:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (80.83%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -1,20 +1,34 @@
+// class Solution {
+//     public int maxDistance(int[] A) {
+//         int n = A.length;
+//         int left = 0, right = 0;
+
+//         for (int i = 0; i < n; i++)
+//             if (A[i] != A[n - 1]) {
+//                 left = i;
+//                 break;
+//             }
+
+//         for (int i = n - 1; i >= 0; i--)
+//             if (A[i] != A[0]) {
+//                 right = i;
+//                 break;
+//             }
+
+//         return Math.max(n - 1 - left, right);
+//     }
+// }
+
+// better with single loop
+
 class Solution {
     public int maxDistance(int[] A) {
-        int n = A.length;
-        int left = 0, right = 0;
+        int j = A.length;
 
-        for (int i = 0; i < n; i++)
-            if (A[i] != A[n - 1]) {
-                left = i;
-                break;
-            }
+        for (int i = 0; i < j; i++)
+            if (A[i] != A[j - 1] || A[j - 1 - i] != A[0])
+                return j - 1 - i;
 
-        for (int i = n - 1; i >= 0; i--)
-            if (A[i] != A[0]) {
-                right = i;
-                break;
-            }
-
-        return Math.max(n - 1 - left, right);
+        return 0;
     }
 }
