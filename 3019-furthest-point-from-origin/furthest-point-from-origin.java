0e03976a88b2a69f5db2680b6dee935b628d0435
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-04-25 00:46:46 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (82.00%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+
+        int Lcount = 0;
+        int Rcount = 0;
+        int count_ = 0;
+
+        for(int i = 0; i < moves.length(); i++){
+            if(moves.charAt(i) == 'L'){
+                Lcount++;
+            }
+            else if(moves.charAt(i) == '_'){
+                count_++;
+            }
+            else{
+                Rcount++;
+            }
+        }
+
+        int res = Lcount > Rcount ? Lcount - Rcount + count_ : (Rcount - Lcount + count_); 
+        
+        return res;
+    }
+}
