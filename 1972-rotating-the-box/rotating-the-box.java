2b1465f2122a52d45d9366f84177febc3a0cced1
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-05-07 02:47:28 +0530

    Time: 6 ms (99.52%) | Memory: 124.6 MB (76.85%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,28 +1,47 @@
+// class Solution {
+//     public char[][] rotateTheBox(char[][] box) {
+//         int rows = box.length;
+//         int col = box[0].length;
+
+//         char[][] res = new char[col][rows];  // length of result box = length of row become length of col and vice versa
+//         for(char[] row : res){
+//             Arrays.fill(row, '.');
+//         }
+
+//         for(int r = 0 ; r < rows; r++){
+//             int i = col - 1;
+//             for(int c = col - 1; c>=0; c--){
+//                 if(box[r][c] == '#'){
+//                     res[i][rows - r - 1] = '#';
+//                     i--;
+//                 }
+//                 else if(box[r][c] == '*'){
+//                     res[c][rows - r - 1] = '*';
+//                     i = c - 1; 
+//                 }
+//             }
+//         }
+//         return res;
+
+        
+//     }
+// }
+
+
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-        int rows = box.length;
-        int col = box[0].length;
-
-        char[][] res = new char[col][rows];  // length of result box = length of row become length of col and vice versa
-        for(char[] row : res){
-            Arrays.fill(row, '.');
-        }
-
-        for(int r = 0 ; r < rows; r++){
-            int i = col - 1;
-            for(int c = col - 1; c>=0; c--){
-                if(box[r][c] == '#'){
-                    res[i][rows - r - 1] = '#';
-                    i--;
-                }
-                else if(box[r][c] == '*'){
-                    res[c][rows - r - 1] = '*';
-                    i = c - 1; 
+        int m = box.length, n = box[0].length;
+        char [][] res = new char[n][m];
+        for (int i = 0; i < m; ++i){
+            for (int j = n - 1, k = n - 1; j >= 0; --j) {
+                res[j][m - i - 1] = 
+                '.';
+                if (box[i][j] != '.') {
+                    k = box[i][j] == '*' ? j : k;
+                    res[k--][m - i - 1] = box[i][j];
                 }
             }
         }
         return res;
-
-        
     }
 }
