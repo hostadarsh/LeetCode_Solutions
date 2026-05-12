05ad006f0f4d2b1309bb903ac2376401c7154b77
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-05-12 23:59:05 +0530

    Time: 6 ms (99.37%) | Memory: 110.6 MB (51.37%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,76 +1,35 @@
-// import java.util.*;
-
-// class Solution {
-//     public int minOperations(int[][] grid, int x) {
-//         List<Integer> all = new ArrayList<>();
-        
-//         for (int[] row : grid) {
-//             for (int num : row) {
-//                 all.add(num);
-//             }
-//         }
-        
-//         int mod = all.get(0) % x;
-//         for (int num : all) {
-//             if (num % x != mod) return -1;
-//         }
-
-//         Collections.sort(all);
-//         int median = all.get(all.size() / 2);
-//         int operations = 0;
-
-//         for (int num : all) {
-//             operations += Math.abs(num - median) / x;
-//         }
-
-//         return operations;
-//     }
-// }
-
-
 class Solution {
     public int minOperations(int[][] grid, int x) {
+        int n = grid.length, m = grid[0].length;
+        int N = n * m;
+        int[] freq = new int[10001];
+        int min = grid[0][0], max = min;
 
-        int m = grid.length, n = grid[0].length;
-
-        int len = m * n;
-        if(len < 2) return 0;
-        
-        int[] count = new int[10001];
-
-        int totalSum = 0;
-        int r = grid[0][0] % x;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                int v = grid[i][j];
-                if(v % x != r) return -1;
-                count[v]++;
-            } 
+        for (int[] row : grid) {
+            for (int val : row) {
+                if ((val - grid[0][0]) % x != 0)
+                    return -1;
+                freq[val]++;
+                min = Math.min(min, val);
+                max = Math.max(max, val);
+            }
         }
-        
-        len = (len + 1) / 2;
-        int total = 0;
-        int median = 0;
 
-        for(int v = 0; v < count.length; ++v){
-            if(count[v] == 0) continue;
-            total += count[v];
-            if(total >= len){
-                median = v;
+        int target = (N + 1) / 2;
+        int acc = 0, median = min;
+
+        for (int i = min; i <= max; i += x) {
+            acc += freq[i];
+            if (acc >= target) {
+                median = i;
                 break;
             }
         }
-        
-        int ans = 0;
-        for(int v = 0; v <= median; ++v){
-            if(count[v] == 0) continue;
-            ans += count[v] * (median - v) / x;
-        }
 
-        for(int v = median + 1; v < count.length; ++v){
-            if(count[v] == 0) continue;
-            ans += count[v] * (v - median) / x;
-        }        
-        return ans;
+        int ops = 0;
+        for (int i = min; i <= max; i += x)
+            ops += Math.abs(i - median) / x * freq[i];
+
+        return ops;
     }
 }
