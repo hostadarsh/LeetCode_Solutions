6cad6d90e0841b9ef6fc2202a431baf360494fab
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-05-15 04:05:06 +0530

    Time: 1 ms (91.05%) | Memory: 44.8 MB (64.95%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+
+        int n = nums.length;
+        int[] freq = new int[n];
+
+        for(int num : nums){
+            if(num < 0 || num > n-1){
+                return false;
+            }
+            freq[num]++;
+        }
+
+        for(int i = 1; i < n-1; i++){
+            if(freq[i] != 1){
+                return false;
+            }
+        }
+
+        return (freq[n-1] == 2);
+
+    }
+}
