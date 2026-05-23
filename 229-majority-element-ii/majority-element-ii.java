151d015859728e1e0f89a101ad4e46a840ced7d5
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-05-23 17:48:37 +0530

    Time: 13 ms (44.94%) | Memory: 53.4 MB (6.14%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,52 +1,21 @@
-// class Solution {
-//     public List<Integer> majorityElement(int[] nums) {
-        
-//         List<Integer> ls = new ArrayList<>();
-//         int s = nums.length;
-
-//         for(int i = 0; i < s; i++ ){
-//             if(ls.size() == 0 || ls.get(0) != nums[i]){
-//                 int count = 0;
-//                 for(int j = 0; j < s; j++){
-//                     if(nums[i] == nums[j]){
-//                         count++;
-//                     }
-//                 }
-//                 if(count > (s/3)){
-//                     ls.add(nums[i]);
-//                 }
-//             }
-
-//             if(ls.size() == 2){
-//                 break;
-//             }
-//         }
-//         return ls;
-
-//     }
-// }
-
-
-
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+       // Arrays.sort(nums);
 
-        List<Integer> ans = new ArrayList<>();
+       List<Integer> ans = new ArrayList<>();
 
-        HashMap<Integer, Integer> mpp = new HashMap<>();
-        int min = (int)(nums.length/3) + 1;
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int i = 0; i< nums.length; i++){
-            
-            int value = mpp.getOrDefault(nums[i],0);
-            mpp.put(nums[i], value+1);
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
+        }
 
-            if(mpp.get(nums[i]) == min){
-                ans.add(nums[i]);
+        for(int key : map.keySet()){
+
+            if(map.get(key) > nums.length / 3){
+                ans.add(key);
             }
-            if(ans.size() == 2){
-                break;
-            }        }
+        }
 
         return ans;
 
