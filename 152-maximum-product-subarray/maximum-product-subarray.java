9feb61b1395c134fdfaf369fc2740d53f50fc377
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-05-23 19:27:39 +0530

    Time: 2 ms (70.45%) | Memory: 47.3 MB (51.59%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,54 +1,25 @@
-// class Solution {
-//     public int maxProduct(int[] arr) {
-//      int result = Integer.MIN_VALUE;
-// 	    for(int i=0;i<arr.length-1;i++) 
-// 	        for(int j=i+1;j<arr.length;j++) {
-// 	            int prod = 1;
-// 	            for(int k=i;k<=j;k++) 
-// 	                prod *= arr[k];
-// 	            result = Math.max(result,prod);
-// 	        }
-// 	   return result; 
-
-//     }
-// }
-
-
-// class Solution {
-//     public int maxProduct(int[] arr) {
-//         int result = Integer.MIN_VALUE;
-//         for(int i = 0; i < arr.length; i++){
-//             int prod = arr[i];
-//             for(int j = i + 1; j < arr.length; j++){
-//                 result = Math.max(prod,result);
-//                 prod = prod * arr[j];
-                
-//             }
-//             result = Math.max(prod,result);
-//         }
-//         return result;
-
-//     }
-// }
-
-
-
 class Solution {
-    public int maxProduct(int[] arr) {
+    public int maxProduct(int[] nums) {
+        
+        int res = nums[0];
+        int minProd = nums[0];
+        int maxProd = nums[0];
 
-       int pref = 1, suff = 1;
-       int ans = Integer.MIN_VALUE;
-       for(int i = 0; i< arr.length; i++){
-            if(pref == 0) pref = 1;
-            if(suff == 0) suff = 1;
+        for(int i = 1; i < nums.length; i++){
+            int curr = nums[i];
 
-            pref *= arr[i];
-            suff *= arr[arr.length -i-1];
+            if(curr < 0){
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
 
-            ans = Math.max(ans, Math.max(pref,suff));
-            
-       }
-       return ans;
+            maxProd = Math.max(curr, maxProd * curr);
+            minProd = Math.min(curr, minProd * curr);
 
+            res = Math.max(res, maxProd);
+        }
+
+        return res;
     }
 }
