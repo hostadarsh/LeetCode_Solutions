226378fd8b960471b5361d9b2c1b070b03628d52
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-05-23 16:42:38 +0530

    Time: 1 ms (99.95%) | Memory: 77.3 MB (47.67%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,78 +1,24 @@
-// Better Approach but time limit exceed
-
-// class Solution {
-//     public int maxSubArray(int[] nums) {
-
-//         int maxSum = Integer.MIN_VALUE;
-        
-//         for(int i = 0; i<nums.length; i++){
-//             int sum = 0;
-//             for(int j = i; j < nums.length; j++ ){
-                             
-//                 sum += nums[j];
-                
-//                maxSum = Math.max(maxSum,sum);
-//             }
-//         }
-//         return maxSum;
-         
-//     }
-// }
-
-
-// class Solution {
-//     public int maxSubArray(int[] nums) {
-
-//         int maxSum = Integer.MIN_VALUE;
-//         int sum = 0;
-        
-//         for(int i = 0; i < nums.length; i++){
-            
-//         sum = sum + nums[i];
-
-//         if(sum > maxSum){
-//             maxSum = sum;
-//         }
-
-//         if(sum < 0){
-//             sum = 0;
-//         }
-
-          
-//         }
-//         return maxSum;
-         
-//     }
-// }
-
-// class Solution {
-//     public int maxSubArray(int[] nums) {
-//         int s = 0, ms = nums[0];
-//         for (int num : nums) {
-//             s += num;
-//             ms = s >ms ? s : ms;
-//             if (s < 0) s = 0;
-//         }
-//         return ms;
-//     }
-// }
-
-
-
-
 class Solution {
     public int maxSubArray(int[] nums) {
-     
-        int ms= nums[0], s = 0;
+        
+        long maxS = Long.MIN_VALUE;
 
-        for(int n : nums){
-            s = s + n;
-           
-            ms = Math.max(ms,s);
+        long sum = 0;
 
-            s= s<0?0:s;
+        for(int i = 0; i < nums.length; i++){
+
+            sum = sum + nums[i];
+
+            if(sum > maxS){
+                maxS = sum;
+            }
+
+            if(sum < 0){
+                sum = 0;
+            }
         }
-        return ms;
+
+        return (int)maxS;
 
     }
 }
