c76b4c8077eced07b3c5e9f79165279931ab628b
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-05-27 01:51:18 +0530

    Time: 1 ms (100.00%) | Memory: 43.2 MB (97.88%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,22 +1,50 @@
+// class Solution {
+//     public int numberOfSpecialChars(String word) {
+//         boolean[] small = new boolean[26];
+//         boolean[] large = new boolean[26];
+
+//         int count = 0;
+
+//         for(char ch : word.toCharArray()){
+//             if(ch >= 'a' && ch <= 'z'){
+//                 small[ch - 'a'] = true;
+//             }
+//             else{
+//                 large[ch - 'A'] = true;
+//             }
+//         }
+
+//         for(int i = 0; i < 26; i++){
+//             if(small[i] && large[i]){
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
+
+
+
+
 class Solution {
     public int numberOfSpecialChars(String word) {
-        boolean[] small = new boolean[26];
-        boolean[] large = new boolean[26];
-
+        
+        int[] freq = new int[26];
         int count = 0;
 
         for(char ch : word.toCharArray()){
             if(ch >= 'a' && ch <= 'z'){
-                small[ch - 'a'] = true;
+                freq[ch - 'a'] |= 1;
             }
             else{
-                large[ch - 'A'] = true;
+                freq[ch - 'A'] |= 2;
             }
         }
 
         for(int i = 0; i < 26; i++){
-            if(small[i] && large[i]){
-                count++;
+            if(freq[i] == 3){
+                count ++;
             }
         }
 
