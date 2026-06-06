edbcfd59e5bec55a0d2889b34ccf247535ea62af
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-06-06 19:13:44 +0530

    Time: 14 ms (20.91%) | Memory: 62.5 MB (18.99%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -8,117 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode sortList(ListNode head) {
-
-//         List<Integer> arr = new ArrayList<>();
-
-//         ListNode temp = head;
-
-//         while(temp != null){
-//             arr.add(temp.val);
-//             temp = temp.next;
-//         }
-
-//         Collections.sort(arr);
-
-//         temp = head;
-
-//         for(int i = 0; i < arr.size(); i++){
-//             temp.val = arr.get(i);
-//             temp = temp.next;
-//         }
-
-//         return head;
-        
-//     }
-// }
-
-
-// class Solution {
-//     public ListNode sortList(ListNode head) {
-
-//         List<Integer> arr = new ArrayList<>();
-
-//         ListNode temp = head;
-
-//         while(temp != null){
-//             arr.add(temp.val);
-//             temp = temp.next;
-//         }
-
-//         Collections.sort(arr);
-
-//         temp = head;
-//         int i = 0;
-//         while(temp !=  null){
-//             temp.val = arr.get(i++);
-//             temp = temp.next;
-//         }
-//         return head; 
-//     }
-// }
-
-
 class Solution {
     public ListNode sortList(ListNode head) {
+        ArrayList<Integer> arr = new ArrayList<>();
 
-        if( head == null || head.next == null) return head;
+        ListNode temp = head;
 
-        ListNode middle = findMiddle(head);
-        
-        ListNode right = middle.next;
-        middle.next = null;
-        ListNode left = head;
-
-        left = sortList(left);
-        right = sortList(right);
-
-        return mergeTwoSortedLinkedList(left,right);
-
-    }
-
-    public ListNode findMiddle(ListNode head){
-        if(head == null || head.next == null) return head;
-
-        ListNode slow = head;
-        ListNode fast = head.next;
-
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+        while(temp != null){
+            arr.add(temp.val);
+            temp = temp.next;
         }
 
-        return slow; // middle node
-    }
+        Collections.sort(arr);
 
-public ListNode mergeTwoSortedLinkedList(ListNode list1, ListNode list2){
+        temp = head;
+        int i = 0;
 
-    ListNode dummyNode = new ListNode(0);
-    ListNode temp = dummyNode;
-
-    while(list1 != null && list2 != null){
-        if(list1.val <= list2.val){
-            temp.next = list1;
-            list1 = list1.next;
+        while(temp != null){
+            temp.val = arr.get(i++);
+            temp = temp.next;
         }
-        else{
-            temp.next = list2;
-            list2 = list2.next;
-        }
-        temp = temp.next;
+
+        return head;
     }
-
-    if(list1 != null){
-        temp.next = list1;
-    }
-    else{
-        temp.next = list2;
-    }
-
-    return dummyNode.next;
-
-}
-
-
 }
