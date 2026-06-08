921fa90493bf870eaaf0015d83ea7f2fed2d4048
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-06-08 22:33:06 +0530

    Time: 15 ms (15.65%) | Memory: 160.7 MB (15.95%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,20 +1,35 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int n = nums.length;
-        int[] less = new int[n], high = new int[n], result = new int[n];
-        int count = 0, j = 0, k = 0;
+        List<Integer> lesser = new ArrayList<>();
+        List<Integer> pivotCount = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
 
-        for(int num : nums) {
-            if(num < pivot) less[j++] = num;
-            else if(num == pivot) count++;
-            else high[k++] = num;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < pivot){
+                lesser.add(nums[i]);
+            }
+            else if(nums[i] > pivot){
+                greater.add(nums[i]);
+            }
+            else{
+                pivotCount.add(pivot);
+            }
         }
 
-        int index = 0;
-        for(int a = 0; a < j; a++) result[index++] = less[a];
-        for(int a = 0; a < count; a++) result[index++] = pivot;
-        for(int a = 0; a < k; a++) result[index++] = high[a];
+        int [] ans = new int [nums.length];
 
-        return result;
+        int i = 0;
+
+        for(int n : lesser){
+            ans[i++] = n;
+        }
+        for(int n : pivotCount){
+            ans[i++] = n;
+        }
+        for(int n : greater){
+            ans[i++] = n;
+        }
+
+return ans;
     }
 }
