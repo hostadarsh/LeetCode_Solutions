5ddc4e1b7b778d04e05a9e6176da741924a74fd7
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-06-08 22:37:19 +0530

    Time: 4 ms (99.24%) | Memory: 159.9 MB (49.83%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,35 +1,61 @@
+// class Solution {
+//     public int[] pivotArray(int[] nums, int pivot) {
+//         List<Integer> lesser = new ArrayList<>();
+//         List<Integer> pivotCount = new ArrayList<>();
+//         List<Integer> greater = new ArrayList<>();
+
+//         for(int i = 0; i < nums.length; i++){
+//             if(nums[i] < pivot){
+//                 lesser.add(nums[i]);
+//             }
+//             else if(nums[i] > pivot){
+//                 greater.add(nums[i]);
+//             }
+//             else{
+//                 pivotCount.add(pivot);
+//             }
+//         }
+
+//         int [] ans = new int [nums.length];
+
+//         int i = 0;
+
+//         for(int n : lesser){
+//             ans[i++] = n;
+//         }
+//         for(int n : pivotCount){
+//             ans[i++] = n;
+//         }
+//         for(int n : greater){
+//             ans[i++] = n;
+//         }
+
+// return ans;
+//     }
+// }
+
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> lesser = new ArrayList<>();
-        List<Integer> pivotCount = new ArrayList<>();
-        List<Integer> greater = new ArrayList<>();
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] < pivot){
-                lesser.add(nums[i]);
+        int[] result = new int[nums.length];
+        int left = 0, right = nums.length - 1;
+        
+        for (int i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+            if (nums[i] < pivot) {
+                result[left] = nums[i];
+                left++;
             }
-            else if(nums[i] > pivot){
-                greater.add(nums[i]);
+            
+            if (nums[j] > pivot) {
+                result[right] = nums[j];
+                right--;
             }
-            else{
-                pivotCount.add(pivot);
-            }
         }
-
-        int [] ans = new int [nums.length];
-
-        int i = 0;
-
-        for(int n : lesser){
-            ans[i++] = n;
+        
+        while (left <= right) {
+            result[left] = pivot;
+            left++;
         }
-        for(int n : pivotCount){
-            ans[i++] = n;
-        }
-        for(int n : greater){
-            ans[i++] = n;
-        }
-
-return ans;
+        
+        return result;
     }
 }
