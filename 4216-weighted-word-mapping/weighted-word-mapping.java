235b7530163639b9533e978963768a8514406dad
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-06-13 15:42:19 +0530

    Time: 2 ms (96.85%) | Memory: 46.6 MB (49.79%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder sb = new StringBuilder();
+
+        for(String word : words){
+            int count = 0;
+            int index;
+            
+
+            for(char ch : word.toCharArray()){
+                count += weights[ch - 'a'];
+            }
+
+            if(count >= 26){
+                index = count % 26;
+            }
+            else{
+                index = count;
+            }
+
+            sb.append((char) ('z' - index));
+        }
+
+        return sb.toString();
+    }
+}
