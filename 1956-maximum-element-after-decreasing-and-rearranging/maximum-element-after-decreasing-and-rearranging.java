71c48d8cf3bebd82879721e1d1f1da2112b1a9fa
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-06-28 11:25:39 +0530

    Time: 9 ms (67.50%) | Memory: 77.6 MB (23.33%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -7,15 +7,16 @@ class Solution {
 
         for(int i = 1; i < arr.length; i++){
             
-            if(arr[i] == arr[i - 1] + 1){
-                continue;
-            }
-            else if(arr[i] > arr[i - 1] && arr[i] != arr[i - 1] + 1 ){
-                arr[i] = arr[i - 1] + 1;
-            }
-            else{
-                continue;
-            }
+            // if(arr[i] == arr[i - 1] + 1){
+            //     continue;
+            // }
+            // else if(arr[i] > arr[i - 1] && arr[i] != arr[i - 1] + 1 ){
+            //     arr[i] = arr[i - 1] + 1;
+            // }
+            // else{
+            //     continue;
+            // }
+            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
         }
 
         return arr[arr.length - 1];
