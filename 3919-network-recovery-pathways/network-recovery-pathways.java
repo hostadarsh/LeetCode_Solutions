a4092b8e868efaf70fdbbd1980fb5b26ffe8a865
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-07-03 11:06:54 +0530

    Time: 14 ms (98.02%) | Memory: 157.1 MB (95.05%) - LeetSync

--- 3919-network-recovery-pathways/network-recovery-pathways.java
+++ 3919-network-recovery-pathways/network-recovery-pathways.java
@@ -1,89 +1,70 @@
 class Solution {
-    private boolean check(long mid, List<int[]>[] adj, List<Integer> topo,
-                          boolean[] online, long k, int n) {
+    private int[] head, next, to, weight;
+    private long k;
+    private int n;
+    public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
+        this.n = online.length;
+        this.k = k;
 
-        long INF = (long) 1e18;
-        long[] dist = new long[n];
-        Arrays.fill(dist, INF);
-        dist[0] = 0;
+        int m = edges.length;
+        this.head = new int[n];
+        this.next = new int[m];
+        this.to = new int[m];
+        this.weight = new int[m];
+        Arrays.fill(head, -1);
+        int left = Integer.MAX_VALUE, right = 0;
+        for(int i = 0; i < m; i++) {
+            int a = edges[i][0], b = edges[i][1], c = edges[i][2];
+            if(online[a] && online[b]) {
+                to[i] = b;
+                next[i] = head[a];
+                weight[i] = c;
+                head[a] = i;
 
-        for (int u : topo) {
-            if (dist[u] == INF) continue;
-
-            if (u != 0 && u != n - 1 && !online[u]) continue;
-
-            for (int[] edge : adj[u]) {
-                int v = edge[0];
-                int w = edge[1];
-
-                if (w < mid) continue;
-                if (v != n - 1 && !online[v]) continue;
-
-                dist[v] = Math.min(dist[v], dist[u] + w);
+                if(c > right) right = c;
+                if(c < left) left = c;
             }
         }
 
-        return dist[n - 1] <= k;
+        if(!check(0)) return -1;
+
+        while(left < right) {
+            int mid = left + right + 1 >>> 1;
+            if(check(mid)) left = mid;
+            else right = mid - 1;
+        }
+        return left;
     }
 
-    public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
+    private static final int[] queue = new int[50001];
+    private boolean check(int threshold) {
+        long[] dist = new long[n];
+        Arrays.fill(dist, k + 1);
+        dist[0] = 0;
 
-        int n = online.length;
-
-        List<int[]>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++)
-            adj[i] = new ArrayList<>();
-
-        int[] indegree = new int[n];
-
-        int maxEdge = 0;
-
-        for (int[] e : edges) {
-            int u = e[0];
-            int v = e[1];
-            int w = e[2];
-
-            adj[u].add(new int[]{v, w});
-            indegree[v]++;
-            maxEdge = Math.max(maxEdge, w);
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0)
-                q.offer(i);
-        }
-
-        List<Integer> topo = new ArrayList<>();
-
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            topo.add(u);
-
-            for (int[] edge : adj[u]) {
-                int v = edge[0];
-                indegree[v]--;
-
-                if (indegree[v] == 0)
-                    q.offer(v);
+        int read = 0, write = 1;
+        while(read < write) {
+            int current = queue[read++];
+            //if(current.dist > dist[current.index]) continue;
+            for(int i = head[current]; i != -1; i = next[i]) {
+                if(weight[i] < threshold) continue;
+                int nextIndex = to[i];
+                long d = dist[current] + weight[i];
+                if(d < dist[nextIndex]) {
+                    if(nextIndex == n - 1) return true;
+                    dist[nextIndex] = d;
+                    queue[write++] = nextIndex;
+                }
             }
         }
-
-        long low = 0, high = maxEdge;
-        int ans = -1;
-
-        while (low <= high) {
-            long mid = low + (high - low) / 2;
-
-            if (check(mid, adj, topo, online, k, n)) {
-                ans = (int) mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        return false;
+    }
+    private static final class Node {
+        private final int index;
+        private final long dist;
+        private Node(int index, long dist) {
+            this.index = index;
+            this.dist = dist;
         }
-
-        return ans;
     }
 }
