25198af53a69741c8997fa6bcdfe15d189d9c9d0
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-08-24 22:54:31 +0530

    Time: 3 ms (100.00%) | Memory: 92.3 MB (26.38%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -1,13 +1,33 @@
+// class Solution {
+//     public int stoneGameVIII(int[] A) {
+//         int n = A.length;
+//         for (int i = 1; i < n; i++)
+//             A[i] += A[i - 1];
+
+//         int ans = A[n - 1];
+//         for (int i = n - 2; i > 0; i--)
+//             ans = Math.max(ans, A[i] - ans);
+
+//         return ans;
+//     }
+// }
+
 class Solution {
-    public int stoneGameVIII(int[] A) {
-        int n = A.length;
-        for (int i = 1; i < n; i++)
-            A[i] += A[i - 1];
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        int[] prefixSum = new int[n];
+        prefixSum[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i];
+        }
 
-        int ans = A[n - 1];
-        for (int i = n - 2; i > 0; i--)
-            ans = Math.max(ans, A[i] - ans);
+        // maxDiff tracks the maximum score difference a player can achieve 
+        // starting from index i to the end.
+        int maxDiff = prefixSum[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            maxDiff = Math.max(maxDiff, prefixSum[i] - maxDiff);
+        }
 
-        return ans;
+        return maxDiff;
     }
 }
