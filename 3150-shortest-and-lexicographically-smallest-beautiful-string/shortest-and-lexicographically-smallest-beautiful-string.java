49a8a71bb88664d918c48b2ab73ab54842683258
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-08-26 16:16:36 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (87.25%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -36,41 +36,74 @@
 //     }
 // }
 
-public class Solution {
+// public class Solution {
 
+//     public String shortestBeautifulSubstring(String s, int k) {
+//         char[] input = s.toCharArray();
+
+//         int head = -1;
+//         int tail = -1;
+
+//         int back = 0;
+//         int countOnes = 0;
+
+//         for (int front = 0; front < input.length; ++front) {
+//             countOnes += input[front] - '0';
+//             if (countOnes < k) {
+//                 continue;
+//             }
+
+//             while (back < front && input[back] == '0') {
+//                 countOnes -= input[back] - '0';
+//                 ++back;
+//             }
+
+//             if (head == -1 || head - tail + 1 > front - back + 1) {
+//                 head = front;
+//                 tail = back;
+//             } else if (head - tail + 1 == front - back + 1
+//                     && s.substring(tail, head + 1).compareTo(s.substring(back, front + 1)) > 0) {
+//                 head = front;
+//                 tail = back;
+//             }
+//             while (back < front && countOnes == k) {
+//                 countOnes -= input[back] - '0';
+//                 ++back;
+//             }
+//         }
+//         return head != -1 ? s.substring(tail, head + 1) : "";
+//     }
+// }
+
+class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        char[] input = s.toCharArray();
-
-        int head = -1;
-        int tail = -1;
-
-        int back = 0;
-        int countOnes = 0;
-
-        for (int front = 0; front < input.length; ++front) {
-            countOnes += input[front] - '0';
-            if (countOnes < k) {
-                continue;
+        int n = s.length();
+        int count = 0;
+        int lo = 0;
+        String ans = "";
+        for(int i = 0; i < n; i++){
+            //Update counter
+            if (s.charAt(i) == '1'){
+                count++;
             }
-
-            while (back < front && input[back] == '0') {
-                countOnes -= input[back] - '0';
-                ++back;
+            //Update left pointer if counter > k Or has prefix '0'
+            while(count > k || (count == k && s.charAt(lo) == '0')){
+                if(s.charAt(lo) == '1'){
+                    count--;
+                }
+                lo++;
             }
-
-            if (head == -1 || head - tail + 1 > front - back + 1) {
-                head = front;
-                tail = back;
-            } else if (head - tail + 1 == front - back + 1
-                    && s.substring(tail, head + 1).compareTo(s.substring(back, front + 1)) > 0) {
-                head = front;
-                tail = back;
-            }
-            while (back < front && countOnes == k) {
-                countOnes -= input[back] - '0';
-                ++back;
+            // Update ans
+            if(count == k){
+                String sub = s.substring(lo, i+1);
+                if( ans == "" ||
+                    sub.length() < ans.length() || 
+                  ((sub.compareTo(ans) < 0) && (sub.length() == ans.length()))
+                ){
+                    ans = sub;
+                }
             }
         }
-        return head != -1 ? s.substring(tail, head + 1) : "";
+        return ans;
     }
 }
