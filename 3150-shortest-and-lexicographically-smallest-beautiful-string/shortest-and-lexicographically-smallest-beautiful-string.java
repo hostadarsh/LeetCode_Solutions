f6ebd964f29af4f75e49815b4f9213373fc6150e
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-08-26 16:16:04 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (58.39%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,37 +1,76 @@
-class Solution {
+// class Solution {
+//     public String shortestBeautifulSubstring(String s, int k) {
+//         String ans = "";
+//         int n = s.length();
+
+//         for (int i = 0; i < n; i++) {
+
+//             int oneCnt = 0;
+//             StringBuilder cur = new StringBuilder();
+
+//             for (int j = i; j < n; j++) {
+
+//                 cur.append(s.charAt(j));
+
+//                 if (s.charAt(j) == '1')
+//                     oneCnt++;
+
+//                 // More than k ones can never become valid again
+//                 if (oneCnt > k)
+//                     break;
+
+//                 if (oneCnt == k) {
+//                     String curStr = cur.toString();
+
+//                     if (ans.isEmpty() ||
+//                         curStr.length() < ans.length() ||
+//                         (curStr.length() == ans.length() && curStr.compareTo(ans) < 0)) {
+
+//                         ans = curStr;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+public class Solution {
+
     public String shortestBeautifulSubstring(String s, int k) {
-        String ans = "";
-        int n = s.length();
+        char[] input = s.toCharArray();
 
-        for (int i = 0; i < n; i++) {
+        int head = -1;
+        int tail = -1;
 
-            int oneCnt = 0;
-            StringBuilder cur = new StringBuilder();
+        int back = 0;
+        int countOnes = 0;
 
-            for (int j = i; j < n; j++) {
+        for (int front = 0; front < input.length; ++front) {
+            countOnes += input[front] - '0';
+            if (countOnes < k) {
+                continue;
+            }
 
-                cur.append(s.charAt(j));
+            while (back < front && input[back] == '0') {
+                countOnes -= input[back] - '0';
+                ++back;
+            }
 
-                if (s.charAt(j) == '1')
-                    oneCnt++;
-
-                // More than k ones can never become valid again
-                if (oneCnt > k)
-                    break;
-
-                if (oneCnt == k) {
-                    String curStr = cur.toString();
-
-                    if (ans.isEmpty() ||
-                        curStr.length() < ans.length() ||
-                        (curStr.length() == ans.length() && curStr.compareTo(ans) < 0)) {
-
-                        ans = curStr;
-                    }
-                }
+            if (head == -1 || head - tail + 1 > front - back + 1) {
+                head = front;
+                tail = back;
+            } else if (head - tail + 1 == front - back + 1
+                    && s.substring(tail, head + 1).compareTo(s.substring(back, front + 1)) > 0) {
+                head = front;
+                tail = back;
+            }
+            while (back < front && countOnes == k) {
+                countOnes -= input[back] - '0';
+                ++back;
             }
         }
-
-        return ans;
+        return head != -1 ? s.substring(tail, head + 1) : "";
     }
 }
