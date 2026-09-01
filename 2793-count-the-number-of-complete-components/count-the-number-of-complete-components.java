919bb41e6e854f9b01f702c5280a8397d39d1d42
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-09-02 00:33:01 +0530

    Time: 4 ms (99.27%) | Memory: 47.6 MB (60.29%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,40 +1,106 @@
+// class Solution {
+//     static int V, D;
+
+//     public int countCompleteComponents(int n, int[][] edges) {
+//         List<Integer>[] A = new ArrayList[n];
+//         Arrays.setAll(A, _ -> new ArrayList<>());
+
+//         for (int[] e : edges) {
+//             A[e[0]].add(e[1]);
+//             A[e[1]].add(e[0]);
+//         }
+
+//         boolean[] vis = new boolean[n];
+//         int res = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             boolean state = vis[i];
+
+//             if (!state) {
+//                 V = 0; D = 0;
+
+//                 dfs(i, A, vis);
+
+//                 if (D == V * (V - 1)) res++;
+//             }
+//         }
+
+//         return res;
+//     }
+
+//     private void dfs(int x, List<Integer>[] A, boolean[] vis) {
+//         V++;
+//         D += A[x].size();
+//         vis[x] = true;
+
+//         for (int state : A[x])
+//             if (!vis[state])
+//                 dfs(state, A, vis);
+//     }
+// }
+
+
 class Solution {
-    static int V, D;
+    int[] rank;
+    int[] parent;
 
-    public int countCompleteComponents(int n, int[][] edges) {
-        List<Integer>[] A = new ArrayList[n];
-        Arrays.setAll(A, _ -> new ArrayList<>());
+    public int find(int u)
+    {
+        if(u==parent[u]) return u;
 
-        for (int[] e : edges) {
-            A[e[0]].add(e[1]);
-            A[e[1]].add(e[0]);
-        }
-
-        boolean[] vis = new boolean[n];
-        int res = 0;
-
-        for (int i = 0; i < n; i++) {
-            boolean state = vis[i];
-
-            if (!state) {
-                V = 0; D = 0;
-
-                dfs(i, A, vis);
-
-                if (D == V * (V - 1)) res++;
-            }
-        }
-
-        return res;
+        return parent[u]=find(parent[u]);
     }
+    public void union(int u,int v)
+    {
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv) return;
 
-    private void dfs(int x, List<Integer>[] A, boolean[] vis) {
-        V++;
-        D += A[x].size();
-        vis[x] = true;
+        if(rank[pu]>rank[pv])
+        {
+            parent[pv]=pu;
+        }
+        else if(rank[pv]>rank[pu])
+        {
+            parent[pu]=pv;
+        }
+        else{
+            parent[pu]=pv;
+            rank[pv]++;
+        }
+    }
+    public int countCompleteComponents(int n, int[][] edges) {
+        rank=new int[n];
+        parent=new int[n];
 
-        for (int state : A[x])
-            if (!vis[state])
-                dfs(state, A, vis);
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+        for(int[] e:edges)
+        {
+            union(e[0],e[1]);
+        }
+
+        int[] nodes = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            nodes[find(i)]++;
+        }
+        int[] edge = new int[n];
+        for(int[] e:edges)
+        {
+            int root=find(e[0]);
+            edge[root]++;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nodes[i]==0) continue;
+            int reqedge=nodes[i]*(nodes[i]-1)/2;
+
+            if(reqedge==edge[i]) ans++;
+        }
+        return ans;
     }
 }
