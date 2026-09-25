8a68184b448d2b4ff71a8853d989c82bb8bb98fa
Adarsh Kumar <112804576+hostadarsh@users.noreply.github.com>
2026-09-25 18:12:29 +0530

    Time: 8 ms (92.80%) | Memory: 46.9 MB (89.60%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.java
+++ 1188-brace-expansion-ii/brace-expansion-ii.java
@@ -1,31 +1,76 @@
+// class Solution {
+//     TreeSet<String> ans = new TreeSet<>();
+
+//     void dfs(String s) {
+//         int r = s.indexOf('}');
+
+//         // No braces left
+//         if (r == -1) {
+//             ans.add(s);
+//             return;
+//         }
+
+//         // Find matching '{'
+//         int l = s.lastIndexOf('{', r);
+
+//         String left = s.substring(0, l);
+//         String right = s.substring(r + 1);
+
+//         // Content inside { }
+//         String inside = s.substring(l + 1, r);
+
+//         for (String part : inside.split(",")) {
+//             dfs(left + part + right);
+//         }
+//     }
+
+//     public List<String> braceExpansionII(String expression) {
+//         dfs(expression);
+//         return new ArrayList<>(ans);
+//     }
+// }
+
 class Solution {
+  public List<String> braceExpansionII(String expression) {
+    return dfs(expression, 0, expression.length() - 1);
+  }
+
+  private List<String> dfs(final String expression, int s, int e) {
     TreeSet<String> ans = new TreeSet<>();
+    List<List<String>> groups = new ArrayList<>();
+    groups.add(new ArrayList<>());
+    int layer = 0;
+    int left = 0;
 
-    void dfs(String s) {
-        int r = s.indexOf('}');
+    for (int i = s; i <= e; ++i)
+      if (expression.charAt(i) == '{' && ++layer == 1)
+        left = i + 1;
+      else if (expression.charAt(i) == '}' && --layer == 0)
+        merge(groups, dfs(expression, left, i - 1));
+      else if (expression.charAt(i) == ',' && layer == 0)
+        groups.add(new ArrayList<>());
+      else if (layer == 0)
+        merge(groups, new ArrayList<>(List.of(String.valueOf(expression.charAt(i)))));
 
-        // No braces left
-        if (r == -1) {
-            ans.add(s);
-            return;
-        }
+    for (final List<String> group : groups)
+      for (final String word : group)
+        ans.add(word);
 
-        // Find matching '{'
-        int l = s.lastIndexOf('{', r);
+    return new ArrayList<>(ans);
+  }
 
-        String left = s.substring(0, l);
-        String right = s.substring(r + 1);
-
-        // Content inside { }
-        String inside = s.substring(l + 1, r);
-
-        for (String part : inside.split(",")) {
-            dfs(left + part + right);
-        }
+  void merge(List<List<String>> groups, List<String> group) {
+    if (groups.get(groups.size() - 1).isEmpty()) {
+      groups.set(groups.size() - 1, group);
+      return;
     }
 
-    public List<String> braceExpansionII(String expression) {
-        dfs(expression);
-        return new ArrayList<>(ans);
-    }
+    List<String> mergedGroup = new ArrayList<>();
+
+    for (final String word1 : groups.get(groups.size() - 1))
+      for (final String word2 : group)
+        mergedGroup.add(word1 + word2);
+
+    groups.set(groups.size() - 1, mergedGroup);
+  }
 }
